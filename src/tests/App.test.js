fe1349decf3b5221b904ed505d253102660947d7
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-08-03 18:59:32 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import App from '../App';
 
@@ -12,12 +12,42 @@ jest.mock('react-router-dom', () => {
   }
 });
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+function renderWithRouter(
+  ui,
+  {
+    route = '/',
+    history = createMemoryHistory({ initialEntries: [route] }),
+  } = {}
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+describe('renderiza o título da página e a barra de navegação', () => {
+  
+  afterEach(cleanup);
+
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('renderiza a barra de navegação', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText(/Home/i);
+    expect(home).toBeInTheDocument();
+    expect(home.href).toBe('http://localhost/');
+
+    const about = getByText(/About/i);
+    expect(about).toBeInTheDocument();
+    expect(about.href).toBe('http://localhost/about');
+
+    const favorite = getByText(/Favorite Pokémons/i);
+    expect(favorite).toBeInTheDocument();
+    expect(favorite.href).toBe('http://localhost/favorites');
+  });
 });
+
