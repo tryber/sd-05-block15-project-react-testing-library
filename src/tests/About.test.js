c8eadcd43d03759aea1579a018cae7f9d313ee6b
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-08-03 18:13:55 -0300

    PokeNaTesting - iniciando o projeto

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+
+afterEach(cleanup);
+
+describe('Requisito 2', () => {
+test('A página "About" deve exibir informações sobre a Pokédex', () => {
+    
+});
+
+})
