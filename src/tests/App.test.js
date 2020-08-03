13896997c80479e9f21addff7200da04f15c0c1e
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-08-03 18:42:15 -0300

    preparaçao das funçoes para lidar com o history em routers

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 import App from '../App';
+
+jest.mock('react-router-dom', () => {
+  const originalModule = jest.requireActual('react-router-dom');
+  return {
+    ...originalModule,
+    BrowserRouter: ({ children }) => (<div>{children}</div>)
+  }
+});
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
