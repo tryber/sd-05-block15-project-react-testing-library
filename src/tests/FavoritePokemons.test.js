575b54cc8ae0e49ced86af9042d244dc1a12a7ae
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-08-04 18:34:18 -0300

    adjusts in Favorite pokemons tests

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('', () => {
+  const { getByText, getByRole } = render(
+    <MemoryRouter>
+      <FavoritePokemons />
+    </MemoryRouter>,
+  );
+  expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+});
