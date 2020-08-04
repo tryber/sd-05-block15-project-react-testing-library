8767a0993412e29044f329f583e7a7d553c47295
Felipe Vieira <flpvieira86@gmail.com>
2020-08-04 18:59:11 -0300

    teste4

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,15 @@
-test('test3', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+describe('Deve conter mais informações sobre apenas o pokémon selecionado', () => {
+  test('A página deve conter um texto <name> Details, onde <name> é o nome do pokémon', () => {
+    const history = createMemoryHistory();
+    const {  } = render(
+      <Router history={history}>
+        <App />
+      </Router>,
+    );
+  });
+});
