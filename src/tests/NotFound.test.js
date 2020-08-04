77c9300dec41ab89b63aa91c20708f19738f8f04
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-08-04 01:01:49 -0300

    Not found test done

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('testes no NotFound', () => {
+  test('testando se A página contém heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const selectH2 = document.querySelector('h2');
+    expect(selectH2).toBeInTheDocument();
+    expect(selectH2).toHaveTextContent('Page requested not found');
+  });
+});
