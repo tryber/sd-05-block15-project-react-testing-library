faa2998a0f6dac3f3c9db6591073b42aa38eb80e
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-08-04 17:18:22 -0300

    beginning req5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, fireEvent, render } from '@testing-library/react';
+import App from '.App.js';
+
+afterEach(cleanup);
+
+// test('Ao apertar o botão de próximo, a página deve exibir o próximo pokémon da lista', () => {
+//   const { getByText } = render(<App />);
+//   const textNext = getByText(/Próximo pokémon/i);
+//   expect(textNext).toBeInTheDocument();
+//   fireEvent.click
+// });
