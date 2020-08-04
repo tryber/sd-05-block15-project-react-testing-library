bb236666999d9cad605d9150eedd372fc6145b82
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-08-04 13:30:01 -0300

    about testing working, false positives tested

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, render } from '@testing-library/react';
+import About from '../components/About';
+
+afterEach(cleanup);
+
+it('tests the content of About page', () => {
+  const { queryByText, container } = render(<About />);
+  const aboutTag = queryByText('About Pokédex');
+  expect(aboutTag).toBeInTheDocument();
+  expect(aboutTag.tagName).toBe('H3');
+  expect(container.querySelectorAll('p').length).toBe(2);
+  expect(container.querySelector('img')).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
