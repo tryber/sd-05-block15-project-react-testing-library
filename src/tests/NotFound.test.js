3a402a65a63453f13908c7bcd137d15fc2736ae1
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-08-04 01:31:56 -0300

    Not found done

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { cleanup } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import NotFound from '../components/NotFound';
 
 describe('testes no NotFound', () => {
+  afterEach(cleanup);
   test('testando se A página contém heading h2 com o texto Page requested not found', () => {
     renderWithRouter(<NotFound />);
     const selectH2 = document.querySelector('h2');
     expect(selectH2).toBeInTheDocument();
     expect(selectH2).toHaveTextContent('Page requested not found');
+  });
+
+  test('testando se a imagem correta eh carregada', () => {
+    const { getAllByRole } = renderWithRouter(<NotFound />);
+    const getImg = getAllByRole('img');
+   // console.log(getImg[1].getAttributeNames());
+    expect(getImg[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
   });
 });
