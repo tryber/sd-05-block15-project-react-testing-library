e6af5ad607b31d2c95209af8925aca88c180ab9c
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-08-04 17:23:34 -0300

    adjusts

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,14 +4,13 @@ import { render } from '@testing-library/react';
 import About from '../components/About';
 
 test('inf. of Pokedex', () => {
-  const { getByText, getAllByTestId, getByRole } = render(
+  const { getByText, getByRole, container } = render(
     <MemoryRouter>
       <About />
     </MemoryRouter>,
   );
   expect(getByText('About Pokédex')).toBeInTheDocument();
-  const paragraph = getAllByTestId('paragraph');
-  expect(paragraph.length).toBe(2);
+  expect(container.querySelectorAll('p').length).toBe(2);
   const imagem = getByRole('img', {
     src:
       'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
