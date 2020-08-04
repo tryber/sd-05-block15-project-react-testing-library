641b642aaf8728d964114578693e5da5ab04f878
Mau <mauricio.sdjr@gmail.com>
2020-08-04 00:27:42 -0300

    implement About.test.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, render } from '@testing-library/react';
+import About from '../components/About';
+
+afterEach(cleanup);
+
+test('Elementos no componente About', () => {
+  const { queryByText, container } = render(<About />);
+  const aboutTag = queryByText('About Pokédex');
+  expect(aboutTag).toBeInTheDocument();
+  expect(aboutTag.tagName).toBe('H2');
+  expect(container.querySelectorAll('p').length).toBe(2);
+  expect(container.querySelector('img')).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
