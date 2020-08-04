3016d228d10d90e2bf809d09b9adedb63fb01280
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-08-04 17:29:23 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,14 +1,32 @@
-import { render, cleanup, fireEvent } from '@testing-library/react';
 import React from 'react';
+import { cleanup, fireEvent, render } from '@testing-library/react';
 import App from '../App';
+import About from '../components/About';
 
 afterEach(cleanup);
 
+test('A página deve conter um heading h2 com o texto About Pokédex', () => {
+  const { queryByText, container, getByText, queryByRole, getByAltText } = render(<About />);
+  const aboutPokedexTag = queryByText('About Pokédex');
+  
+  const paragraphContainer = container.querySelectorAll('p');
 
+  expect(aboutPokedexTag).toBeInTheDocument();
 
-describe('Requisito 2', () => {
-test('A página "About" deve exibir informações sobre a Pokédex', () => {
+  expect(container.querySelector('h2')).toBeInTheDocument();
+  expect(aboutPokedexTag.tagName).toBe('H2');
+  
+  expect(paragraphContainer.length).toBe(2);
 
-});
+  expect(getByText(/This application simulates a Pokédex, a digital encliclopedia containing all Pokémons/i)).toBeInTheDocument();
 
+  expect(getByText(/One can filter Pokémons by type, and see more details for each one of them/i)).toBeInTheDocument();
+
+  expect(queryByRole('img')).toBeInTheDocument();
+
+  expect((queryByRole('img'))).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+
+  expect(getByAltText('Pokédex')).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+
+  expect(getByAltText('Pokédex').tagName).toMatch(/img/i);
 })
