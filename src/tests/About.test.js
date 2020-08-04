e6c10331644b4c3b64862cadf4f38ddd3208857c
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-08-04 16:23:03 -0300

    implementaçoes about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -9,9 +9,18 @@ describe('teste do arquivo about', () => {
   test('Página about informa sobre a pokédex', () => {
     const { getByText, getByRole } = render(<About />);
     const title = getByText(/About Pokédex/);
-    console.log(title);
     expect(title).toBeInTheDocument();
-    expect(title.type).toBe('h2');
+
+    const paragraph1 = getByText(/This application simulates a Pokédex/);
+    expect(paragraph1).toBeInTheDocument();
+
+    const paragraph2 = getByText(/One can filter Pokémons by type/);
+    expect(paragraph2).toBeInTheDocument();
+    
+    const image = getByRole('img');
+    expect(image.src).toBe(
+      'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
+    );
     
     
   });
