e01505cf25be28066b906a8e2af20e7c0c465736
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-08-04 15:53:47 -0300

    codeclimate

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { cleanup, fireEvent, render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import FavoritePokemons from '../components/FavoritePokemons';
 import App from '../App';
-import { MemoryRouter } from 'react-router-dom';
 
 afterEach(cleanup);
 
