e52de7f21587cb16bf689b4c63e258335f58d449
rfreitasbatista <rfreitasbatista@gmail.com>
2020-08-04 15:39:48 -0300

    Inicio de projeto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,3 +12,13 @@ test('renders a reading with the text `PokÃ©dex`', () => {
   const heading = getByText(/PokÃ©dex/i);
   expect(heading).toBeInTheDocument();
 });
+
+test('shows the Pokédex when the route is `/`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
