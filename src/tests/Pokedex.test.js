e454162c666d2bcf02ff66d5ba3930ee9f67aeb3
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-08-04 20:49:33 -0300

    Testing Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,103 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import { Pokedex } from '../components';
+import pokemons from '../data';
+
+const typeList = [...new Set(pokemons.reduce((types, { type }) => [...types, type], []))];
+
+const falsePkm = {
+  id: 666,
+  name: 'Capeta',
+  type: 'Infernal',
+  averageWeight: {
+    value: '666',
+    measurementUnit: 'kg',
+  },
+  image: '',
+  moreInfo: '',
+  foundAt: [
+    {
+      location: '',
+      map: '',
+    },
+  ],
+  summary: 'Capeta é um demônio',
+};
+
+const resetHistory = (pkm = pokemons) => {
+  const {
+    getByText,
+    container,
+    getByRole,
+    getAllByText,
+  } = render(
+    <MemoryRouter>
+      <Pokedex pokemons={pkm} isPokemonFavoriteById={false} />
+    </MemoryRouter>,
+  );
+  return { getByText, container, getByRole, getAllByText };
+};
+
+describe('Routes', () => {
+  afterEach(cleanup);
+
+  test('Checkout next button exists', () => {
+    const { container } = resetHistory();
+    const button = container.querySelector('.pokedex-button');
+    expect(button).toBeInTheDocument();
+    expect(button.innerHTML).toBe('Próximo pokémon');
+  });
+
+  test('Checkout next button functions', () => {
+    const { getByText, container } = resetHistory();
+    const button = getByText(/Próximo pokémon/i);
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+    // Testing click
+    let card = getByText(/Average weight:8.5kg/i);
+    expect(card).toBeInTheDocument();
+    // Testing reset after last pokemon
+    pokemons.forEach(() => {
+      fireEvent.click(button);
+    });
+    card = getByText(/Charmander/i);
+    expect(card).toBeInTheDocument();
+    // Testing one per page
+    card = container.querySelectorAll('.pokemon');
+    expect(card.length).toBe(1);
+  });
+
+  test('checkout filters', () => {
+    const { getByText, getAllByText } = resetHistory();
+    const type = typeList[Math.floor(Math.random() * typeList.length)];
+    let button = getByText(type);
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+
+    const termsCount = getAllByText(type);
+    fireEvent.click(getByText(/Próximo pokémon/i));
+    expect(termsCount.length).toBe(2);
+
+    button = getByText('All');
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+    fireEvent.click(getByText(/Próximo pokémon/i));
+    const charmander = getByText(/Charmander/i);
+    expect(charmander).toBeInTheDocument();
+  });
+
+  test('Dinamic Filters', () => {
+    pokemons.push(falsePkm);
+    const { getByText, container } = resetHistory(pokemons);
+    let button = getByText(falsePkm.type);
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+
+    button = container.querySelector('.pokedex-button');
+    expect(button.disabled).toBe(true);
+
+    button = getByText('All');
+    expect(button).toBeInTheDocument();
+  });
+});
