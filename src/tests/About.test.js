20f9d0d942791afc705dea288ea6fe335ec02122
Lucas Costa Ventura <lucascv333@gmail.com>
2020-08-04 16:52:47 -0300

    Criando testes dentro de About.test.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+
+describe('renders about page', () => {
+  afterEach(cleanup);
+  test('navigating from home to about', () => {
+    const { getByText, getByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const paginaInicial = getByText(/about/i);
+    expect(paginaInicial).toBeInTheDocument();
+
+    fireEvent.click(getByText(/about/i));
+
+    const heading = getByText(/about pokédex/i);
+    expect(heading).toBeInTheDocument();
+
+    const p1 = getByText(/This application simulates a Pokédex, a digital encliclopedia containing all Pokémons/);
+    expect(p1).toBeInTheDocument();
+
+    const p2 = getByText(/One can filter Pokémons by type, and see more details for each one of them/);
+    expect(p2).toBeInTheDocument();
+
+    const image = getByRole('img');
+    expect(image.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+})
