c3d7c0638198807d6505c9f309ab310599666974
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-08-04 17:22:17 -0300

    App.js Done

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('test all App', () => {
+
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+  
+  test('shows the Home Page when the route is `/`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Home/i));
+    const pathname = history.location.pathname;
+    expect(pathname).toBe('/');
+    const heading = getByText(/Encountered pokémons/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('shows the About Page when the route is `/about`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/About/i));
+    const pathname = history.location.pathname;
+    expect(pathname).toBe('/about');
+    const heading = getByText(/About Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('shows the About Page when the route is `/favorite`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const pathname = history.location.pathname;
+    expect(pathname).toBe('/favorites');
+    const heading = getByText(/Favorite pokémons/);
+    expect(heading).toBeInTheDocument();
+  });
 });
+
