6179feb81cf54fef300f24fc3fad5fd1c554e1bb
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-08-04 18:03:05 -0300

    req5 tested type button

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -6,11 +6,25 @@ import { MemoryRouter } from 'react-router-dom';
 afterEach(cleanup);
 
 test('Ao apertar o botão de próximo, a página deve exibir o próximo pokémon da lista', () => {
-  const { getByTestId } = render(<MemoryRouter><App /></MemoryRouter>);
-  // const textNext = getByText(/Próximo pokémon/i);
-  // expect(textNext).toBeInTheDocument();
+  const { getByTestId, getByText, getAllByText } = render(<MemoryRouter><App /></MemoryRouter>);
+  // botao
   const btnNext = getByTestId('next-pokemon');
   expect(btnNext).toBeInTheDocument();
+  // cliques sucessivos, volta no começo, mostra apenas um
+  const pokemonsNames = ['Charmander', 'Caterpie', 'Ekans', 'Alakazam', 'Mew', 'Rapidash', 'Snorlax', 'Dragonair', 'Pikachu'];
+  for (let i = 0; i < pokemonsNames.length; i += 1) {
+    fireEvent.click(getByText(/próximo pokémon/i));
+    expect(getByText(pokemonsNames[i])).toBeInTheDocument();
+    expect(getAllByText(/Average weight/i)).toHaveLength(1);
+  }
+});
 
-  // fireEvent.click
+test('A Pokédex deve exibir apenas um pokémon por vez', () => {
+  const { getAllByTestId, getByText } = render(<MemoryRouter><App /></MemoryRouter>);
+  // botao de filtro
+  const btnFilter = getAllByTestId('pokemon-type-button');
+  expect(btnFilter.length).toBe(7);
+  // cliques sucessivos com filtro, exemplo de um tipo
+  fireEvent.click(getByText(/Psychic/i));
+  expect(getByText(/Alakazam/i)).toBeInTheDocument();
 });
