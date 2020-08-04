9171bcdfa9625c5282b2d7373d657f9d01f4c8a3
Lucas Costa Ventura <lucascv333@gmail.com>
2020-08-04 17:47:55 -0300

    testando FavoritePokemons.test.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testes do arquivo FavoritePokemons.js', () => {
+  afterEach(cleanup);
+  test('Mensagem caso nao tenha pokémons favoritados', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <FavoritePokemons />
+      </MemoryRouter>
+    );
+
+    const msg = getByText(/No favorite pokemon found/);
+    expect(msg).toBeInTheDocument();
+  })
+
+  test('A página deve exibir todos os cards de pokémons favoritados', () => {
+    const { getByText, getByLabelText, getAllByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    fireEvent.click(getByText(/bug/i));
+    fireEvent.click(getByText(/more details/i));
+    fireEvent.click(getByLabelText(/pokémon favoritado/i));
+    fireEvent.click(getByText(/home/i));
+    fireEvent.click(getByText(/fire/i));
+    fireEvent.click(getByText(/more details/i));
+    fireEvent.click(getByLabelText(/pokémon favoritado/i));
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    expect(getAllByText(/More details/i).length).toBe(2);
+  });
+})
