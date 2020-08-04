9c59b3e0a39bd0bd804c7fa614ca08cd00a0e22c
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-08-04 16:49:43 -0300

    cc adjusts

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -7,11 +7,14 @@ test('inf. of Pokedex', () => {
   const { getByText, getAllByTestId, getByRole } = render(
     <MemoryRouter>
       <About />
-    </MemoryRouter>,
+    </MemoryRouter>
   );
   expect(getByText('About Pokédex')).toBeInTheDocument();
   const paragraph = getAllByTestId('paragraph');
   expect(paragraph.length).toBe(2);
-  const imagem = getByRole('img', {src:'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'})
+  const imagem = getByRole('img', {
+    src:
+      'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+  });
   expect(imagem).toBeInTheDocument();
 });
