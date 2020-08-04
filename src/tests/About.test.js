3057db2d55116b0c33de35ea5771dfc4da40e00b
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-08-04 17:56:47 -0300

    hotfix codeclimate

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,21 +1,8 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render, fireEvent } from '@testing-library/react';
 import renderWithRouter from '../services/renderWithRouter';
 import { About } from '../components';
-import App from '../App';
 
 describe('test all |About', () => {
-  // test('renders a reading with the text `Pokédex`', () => {
-  //   const { getByText } = render(
-  //     <MemoryRouter>
-  //       <App />
-  //     </MemoryRouter>,
-  //   );
-  //   const heading = getByText(/Pokédex/i);
-  //   expect(heading).toBeInTheDocument();
-  // });
-
   test('Verify if has h2 heading', () => {
     const { getByRole } = renderWithRouter(<About />);
     const h2 = getByRole('heading');
