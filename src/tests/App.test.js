a8f81f3a19171cfd7745cbcf72ccfcdbfc4e11c9
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-08-04 19:25:32 -0300

    CC

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,7 +5,7 @@ import { Router } from 'react-router-dom';
 import { render, cleanup, fireEvent } from '@testing-library/react';
 import App from '../App';
 
-// mock BrowserRouter para resetar o histórico de navegação
+// mock BrowserRouter para resetar o histórico de navegação 
 // toda vez que entra na página. Substituindo o BrowserRouter.
 // Agora, retorna só uma div no lugar dele.
 
@@ -18,12 +18,12 @@ import App from '../App';
 //   }
 // })
 
-//seta histórico novo de navegação ou, passado um parâmetro, seta o path onde está
-//criação de um elemento encapsulado pelo Router, com um histórico novo, a cada teste
-//que for feito, ou com um histórico diferente que se possa adicionar
+// seta histórico novo de navegação ou, passado um parâmetro, seta o path onde está
+// criação de um elemento encapsulado pelo Router, com um histórico novo, a cada teste
+// que for feito, ou com um histórico diferente que se possa adicionar
 
 function renderWithRouter(ui, { route = '/', history = createMemoryHistory(
-{ initialEntries: [route] }) } = {},) {
+{ initialEntries: [route] }) } = {} ) {
   return {
     ...render(<Router history={history}>{ui}</Router>),
     history,
@@ -43,7 +43,7 @@ describe('Testes da página inicial', () => {
     expect(heading).toBeInTheDocument();
   });
 
-  describe('No topo da aplicação, deve haver um conjunto fixo de links de navegação', () => {    
+  describe('No topo da aplicação, deve haver um conjunto fixo de links de navegação', () => {
 
     test('O primeiro link deve possuir o texto Home com a URL /', () => {
       const { getByText, history } = renderWithRouter(<App />);
@@ -61,15 +61,15 @@ describe('Testes da página inicial', () => {
       expect(path).toBe('/about');
       const about = getByText(/about pokédex/i);
       expect(about).toBeInTheDocument();
-    })
-      
+    });
+    
     test('O terceiro link deve possuir o texto Favorite Pokémons com a URL /favorites', () => {
       const { getByText, history } = renderWithRouter(<App />);
       fireEvent.click(getByText(/favorite pokémons/i));
       const path = history.location.pathname;
       expect(path).toBe('/favorites');
       const favorite = getByText(/No favorite pokemon found/i);
-      expect(favorite).toBeInTheDocument();      
+      expect(favorite).toBeInTheDocument();
     });
   });
 });
