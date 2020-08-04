f43cf3f296dead079b943c431f5b3b3e0137f3a9
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-08-04 17:41:51 -0300

    beginning req5, button next pokemon

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { cleanup, fireEvent, render } from '@testing-library/react';
-import App from '.App.js';
+import App from '../App.js';
+import { MemoryRouter } from 'react-router-dom';
 
 afterEach(cleanup);
 
-// test('Ao apertar o botão de próximo, a página deve exibir o próximo pokémon da lista', () => {
-//   const { getByText } = render(<App />);
-//   const textNext = getByText(/Próximo pokémon/i);
-//   expect(textNext).toBeInTheDocument();
-//   fireEvent.click
-// });
+test('Ao apertar o botão de próximo, a página deve exibir o próximo pokémon da lista', () => {
+  const { getByTestId } = render(<MemoryRouter><App /></MemoryRouter>);
+  // const textNext = getByText(/Próximo pokémon/i);
+  // expect(textNext).toBeInTheDocument();
+  const btnNext = getByTestId('next-pokemon');
+  expect(btnNext).toBeInTheDocument();
+
+  // fireEvent.click
+});
