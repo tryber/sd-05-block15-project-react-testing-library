3886757a85caeccb9c9cca2826f0abe3f11fe76a
rfreitasbatista <rfreitasbatista@gmail.com>
2020-08-04 18:26:04 -0300

    Terceiro requisito feito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../services/renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+test('testar se não há pokemons favoritados', () => {
+  const { container } = renderWithRouter(<FavoritePokemons />);
+  const p = container.querySelectorAll('p');
+  expect(...p).toHaveTextContent('No favorite pokemon found');
+});
