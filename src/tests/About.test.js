d70a2f049873fc8f0e324fc6a5ee229ae153625c
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-08-04 17:59:41 -0300

    cc again :(

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -7,12 +7,12 @@ test('inf. of Pokedex', () => {
   const { getByText, getByRole, container } = render(
     <MemoryRouter>
       <About />
-    </MemoryRouter>
+    </MemoryRouter>,
   );
   expect(getByText('About Pokédex')).toBeInTheDocument();
   expect(container.querySelectorAll('p').length).toBe(2);
   const imagem = getByRole('img');
   expect(imagem.src).toBe(
-    'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
+    'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
   );
 });
