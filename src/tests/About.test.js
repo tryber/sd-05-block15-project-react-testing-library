1a22836c85a3df1e202233a2d380c6296b922ce9
marinabrcls <barcelos.marina@gmail.com>
2020-08-04 17:18:07 -0300

    primeiras cositas

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, render } from '@testing-library/react';
+import App from '../App';
+import About from '../components/About';
+
+afterEach(cleanup);
+
+describe('requisito 2', () => {
+  test('A página deve conter um heading h2 com o texto About Pokédex', () => {
+    const { queryByText, container, querySelectorAll, getByText, queryByRole } = 
+    render(<About />);
+    const aboutPokedexTag = queryByText('About Pokédex');
+    const containerParagraph = container.querySelectorAll('p');
+
+    expect(aboutPokedexTag).toBeInTheDocument();
+    expect(container.querySelector('h2')).toBeInTheDocument();
+    expect (aboutPokedexTag.tagName).toBe('H2');
+    expect(containerParagraph.length).toBe(2);
+  });
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, fireEvent, render } from '@testing-library/react';
+import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
+import { MemoryRouter } from 'react-router-dom';
+
+afterEach(cleanup);
+
+test('', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <FavoritePokemons />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+});
+
+test('', () => {
+  const { getByText, getByLabelText, getAllByText } = render(<MemoryRouter><App /></MemoryRouter>);
+  fireEvent.click(getByText(/more details/i));
+  fireEvent.click(getByLabelText(/pokémon favoritado/i));
+  fireEvent.click(getByText(/Favorite Pokémons/i));
+
+  expect(getAllByText(/More details/i).length).toBe(2);
+});
+
+test('', () => {
+  const { getByText, getByLabelText, getAllByText } = render(<MemoryRouter><App /></MemoryRouter>);
+  fireEvent.click(getByText(/fire/i));
+  fireEvent.click(getByText(/more details/i));
+  fireEvent.click(getByLabelText(/pokémon favoritado/i));
+  fireEvent.click(getByText(/Favorite Pokémons/i));
+
+  expect(getAllByText(/More details/i).length).toBe(2);
+});
