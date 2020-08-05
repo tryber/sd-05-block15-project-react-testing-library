07bccd856d922962d900d0eb13eff86144a44a26
pedrohcalado <pedrocalado22@gmail.com>
2020-08-05 10:14:28 -0300

    Tests on NotFound.test.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('A página deve conter um heading `h2` com o texto `Page requested not found 😭`;', () => {
+  const { getByText, history, container } = renderWithRouter(<App />);
+  history.push('/nao-existe');
+  expect(container.querySelector('h2')).toBeInTheDocument();
+  expect(getByText('Page requested not found')).toBeInTheDocument();
+  expect(container.querySelector('img').src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
