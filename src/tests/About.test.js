de79b31ced55c8cb68b315b263a76602edb0e414
pedrohcalado <pedrocalado22@gmail.com>
2020-08-05 09:11:12 -0300

    Tests on About.test.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+test('A página "About" deve exibir informações sobre a Pokédex', () => {
+  const { getByText, container } = renderWithRouter(<About />);
+  expect(getByText(/About Pokédex/i)).toBeInTheDocument();
+  expect(container.querySelector('h2')).toBeInTheDocument();
+  expect(container.querySelector('img').src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  expect(container.querySelectorAll('p').length).toEqual(2);
+});
