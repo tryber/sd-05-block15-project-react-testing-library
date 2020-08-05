0716a193f148e7f1f33b35b84504640ade4a90f9
César Alves <alvescesar@hotmail.com>
2020-08-04 22:51:13 -0300

    CC Round 1 - Fight!

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -7,13 +7,13 @@ import App from '../App';
 afterEach(cleanup);
 
 describe('Testes de funcionalidade da página Favorite Pokemons', () => {
-  it('Caso a pessoa não tenha pokémons favoritos, a mensagem No favorite pokemon found deve aparecer na tela',() => {
+  it('Caso a pessoa não tenha pokémons favoritos, a mensagem No favorite pokemon found deve aparecer na tela', () => {
     const { getByText } = render(
       <MemoryRouter>
         <FavoritePokemons />
       </MemoryRouter>,
     );
-  
+
     expect(getByText('No favorite pokemon found')).toBeInTheDocument();
   });
 
@@ -21,35 +21,35 @@ describe('Testes de funcionalidade da página Favorite Pokemons', () => {
     const { getByText, getByLabelText } = render(
       <MemoryRouter>
         <App />
-      </MemoryRouter>
+      </MemoryRouter>,
     );
 
-  fireEvent.click(getByText(/more details/i));
-  fireEvent.click(getByLabelText(/pokémon favoritado/i));
-  fireEvent.click(getByText(/Favorite Pokémons/i));
+    fireEvent.click(getByText(/more details/i));
+    fireEvent.click(getByLabelText(/pokémon favoritado/i));
+    fireEvent.click(getByText(/Favorite Pokémons/i));
 
-  expect(getByText(/More details/i)).toBeInTheDocument();
+    expect(getByText(/More details/i)).toBeInTheDocument();
   });
 
   it('A página deve exibir todos os cards de pokémons favoritados', () => {
     const { getByText, getAllByText, getByLabelText } = render(
       <MemoryRouter>
         <App />
-      </MemoryRouter>
+      </MemoryRouter>,
     );
-  
+
     localStorage.clear();
 
     fireEvent.click(getByText(/bug/i));
     fireEvent.click(getByText(/more details/i));
     fireEvent.click(getByLabelText(/pokémon favoritado/i));
     fireEvent.click(getByText(/home/i));
-  
+
     fireEvent.click(getByText(/fire/i));
     fireEvent.click(getByText(/more details/i));
     fireEvent.click(getByLabelText(/pokémon favoritado/i));
     fireEvent.click(getByText(/Favorite Pokémons/i));
-  
+
     expect(getAllByText(/More details/i).length).toBe(2);
   });
 });
