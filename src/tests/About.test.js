87bb664a69101db45b4c54f3f7afc4479fab9f08
RenataNovais <rnovaiscs@gmail.com>
2020-08-05 11:09:19 -0300

    more 3 mutants down... :rocket: - requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { About } from '../components';
+
+test('page has a h2 with text About Pokedex', () => {
+  const { getByText } = render(<About />);
+
+  expect(getByText('About Pokédex')).toBeInTheDocument();
+});
+
+test('it has the correct Pokemon img', () => {
+  const { getByAltText } = render(<About />);
+
+  const imgPokedex = getByAltText('Pokédex');
+  expect(imgPokedex).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
