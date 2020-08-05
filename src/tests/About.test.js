26a4b6cc54d58a77491b59248c03bae948dc8f04
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-08-05 18:49:45 -0300

    CodeClimate

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import About from '../components/About';
 import { createMemoryHistory } from 'history';
 import { render } from 'react-dom';
 import { MemoryRouter, Router } from 'react-router-dom';
-import App from '../App';
+import About from '../components/About';
+// import App from '../App';
 
 
 test('A página "About" deve exibir informações sobre a Pokédex', () => {
   const { getByText } = render(
-  <MemoryRouter>
-    <About />
-  </MemoryRouter>,
+    <MemoryRouter>
+      <About />
+    </MemoryRouter>,
   );
   expect(getByText(/About Pokédex/i)).toBeInTheDocument();
 });
 
-const renderWithRouter = (component) => {
+const renderWithRouter = (comp) => {
   const history = createMemoryHistory();
   return {
-    ...render(<Router history={history}>{component}</Router>), history,
+    ...render(<Router history={history}>{comp}</Router>), history,
   };
 };
 
 test('A página deve conter um heading h2 com o texto About Pokédex', () => {
-  const { getByRole } = renderWithRouter(<About />)
-    expect(getByRole('h2', 'About Pokédex').toBeInTheDocument());
+  const { getByRole } = renderWithRouter(<About />);
+  expect(getByRole('h2', 'About Pokédex').toBeInTheDocument());
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import App from '../App';
 import { render } from '@testing-library/react';
+import App from '../App';
 
-const renderWithRouter = (component) => {
+const renderWithRouter = (compon) => {
   const history = createMemoryHistory();
   return {
-    ...render(<Router history={history}>{component}</Router>), history,
+    ...render(<Router history={history}>{compon}</Router>), history,
   };
 };
 
@@ -15,6 +15,6 @@ test('Entrar em uma URL desconhecida exibe a página Not Found', () => {
   const { getByRole } = renderWithRouter(<App />);
 
   expect(getByRole('img',
-  {alt: 'Pikachu crying because the page requested was not found'}
+  { alt: 'Pikachu crying because the page requested was not found' },
   )).toBeInTheDocument();
 });
