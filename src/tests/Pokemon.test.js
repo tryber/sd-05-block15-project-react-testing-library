02e962f9d160908bf5e70bcd9095da45e450b180
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-08-05 00:04:09 -0300

    codeclimate hotfix

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import {
-  render,
-  fireEvent,
-} from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../services/renderWithRouter';
 
@@ -22,26 +19,16 @@ test('should render a Card of Pokémon', () => {
   expect(name).toHaveTextContent('Pikachu');
   expect(weight).toHaveTextContent('Average weight:6.0kg');
   expect(type).toHaveTextContent('Electric');
-  expect(img.src).toBe('https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
-  );
+  expect(img.src).toBe('https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
 });
 
 test('deve abrir o botão More Details com detalhes do Pokémon', () => {
-  const { getByText, history, getByLabelText, getByAltText } = renderWithRouter(
-    <App />,
-  );
-  
-fireEvent.click
-(getByText(/More Details/i));
+  const { getByText, history, getByLabelText, getByAltText } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/More Details/i));
   const pathname = history.location.pathname;
   expect(pathname).toBe('/pokemons/25');
-
-  
-fireEvent.click
-(getByLabelText('Pokémon favoritado?'));
-  
-fireEvent.click
-(getByText('Home'));
+  fireEvent.click(getByLabelText('Pokémon favoritado?'));
+  fireEvent.click(getByText('Home'));
   const pokemonFav = getByAltText('Pikachu is marked as favorite');
   expect(pokemonFav).toBeInTheDocument();
   expect(pokemonFav.src).toBe('http://localhost/star-icon.svg');
