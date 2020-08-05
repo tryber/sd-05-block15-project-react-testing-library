263a8bd5a93e405501a70b419bede22f81b78e8d
Rafael Assad <rafaassadeng@gmail.com>
2020-08-05 16:19:06 -0300

    pokemon test

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+test('Deve ser retornado um card com as informações de determinado pokémon', () => {
+  const { getByTestId } = renderWithRouter(<App />);
+  expect(getByTestId('pokemon-name').innerHTML).toBe('Pikachu');
+  expect(getByTestId('pokemonType').innerHTML).toBe('Electric');
+  expect(getByTestId('pokemon-weight').innerHTML).toBe('Average weight:6.0kg');
+});
