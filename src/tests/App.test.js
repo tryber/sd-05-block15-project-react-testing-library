86d1195b925556fac8105af241e259ab1a828a1b
RenataNovais <rnovaiscs@gmail.com>
2020-08-05 10:15:32 -0300

    3 mutantes down... primeiro requisito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -21,4 +22,46 @@ test('shows the Pokédex when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+// jest.mock('react-router-dom', () => {
+//   const originalModule = jest.requireActual('react-router-dom'); 
+//   return {
+//     ...originalModule,
+//     BrowserRouter: ({ children }) => {
+//       return (
+//         <div>{children}</div>
+//       );
+//     }
+//   };
+// })
+
+// const history = createMemoryHistory();
+//   const { getAllByText } = render(
+//     <Router history={history}>
+//       <App />
+//     </Router>
+//   );
+
+test('every link redirects to the right page', () => {
+  const history = createMemoryHistory();
+  const { getByText } = render(
+    <Router history={history}>
+      <App />
+    </Router>,
+  );
+
+  const homeLink = getByText('Home');
+  fireEvent.click(homeLink);
+  // console.log(history);
+  expect(history.location.pathname).toBe('/');
+
+  const aboutLink = getByText('About');
+  fireEvent.click(aboutLink);
+  // console.log(history);
+  expect(history.location.pathname).toBe('/about');
+
+  const favLinks = getByText('Favorite Pokémons');
+  fireEvent.click(favLinks);
+  expect(history.location.pathname).toBe('/favorites');
 });
