9bb622adbef1077c08126fa746ced2d3a2bb2404
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-08-05 18:45:56 -0300

    NotFound testing

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import About from '../components/About';
+import { createMemoryHistory } from 'history';
+import { render } from 'react-dom';
+import { MemoryRouter, Router } from 'react-router-dom';
+import App from '../App';
+
+
+test('A página "About" deve exibir informações sobre a Pokédex', () => {
+  const { getByText } = render(
+  <MemoryRouter>
+    <About />
+  </MemoryRouter>,
+  );
+  expect(getByText(/About Pokédex/i)).toBeInTheDocument();
+});
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return {
+    ...render(<Router history={history}>{component}</Router>), history,
+  };
+};
+
+test('A página deve conter um heading h2 com o texto About Pokédex', () => {
+  const { getByRole } = renderWithRouter(<About />)
+    expect(getByRole('h2', 'About Pokédex').toBeInTheDocument());
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+import { render } from '@testing-library/react';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return {
+    ...render(<Router history={history}>{component}</Router>), history,
+  };
+};
+
+test('Entrar em uma URL desconhecida exibe a página Not Found', () => {
+  const { getByRole } = renderWithRouter(<App />);
+
+  expect(getByRole('img',
+  {alt: 'Pikachu crying because the page requested was not found'}
+  )).toBeInTheDocument();
+});
