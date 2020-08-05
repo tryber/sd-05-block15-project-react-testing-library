37d43df94103a7dd51ce0cf5bd671a58773efa00
Christian Duessel <duessel@gmail.com>
2020-08-05 11:26:32 -0300

    codeclimate Fix

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { MemoryRouter, Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
-import { fireEvent, cleanup, render, getByRole, getByLabelText } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { fireEvent, cleanup, render } from '@testing-library/react';
 import FavoritePokemons from '../components/FavoritePokemons';
 import App from '../App';
 
 afterEach(cleanup);
 
 test('renders `No favorite pokemon found`', () => {
-  const { getByText, container } = render(
+  const { getByText } = render(
     <MemoryRouter>
       <FavoritePokemons />
     </MemoryRouter>,
   );
-  const NoFav = getByText(`No favorite pokemon found`);
+  const NoFav = getByText('No favorite pokemon found');
   expect(NoFav).toBeInTheDocument();
 });
 
@@ -21,31 +20,31 @@ test('navigating from home to fav pokemon clicking on one favorite', () => {
   const { getByText, getByLabelText, getAllByText, container, history } = render(
     <MemoryRouter>
       <App />
-    </MemoryRouter>
+    </MemoryRouter>,
   );
   fireEvent.click(getByText('More details'));
   fireEvent.click(getByLabelText('Pokémon favoritado?'));
   fireEvent.click(getByText(/Favorite Pokémons/i));
-  const Pika = getByText(`Pikachu`);
+  const Pika = getByText('Pikachu');
   expect(Pika).toBeInTheDocument();
   expect(getAllByText(/More details/i).length).toBe(1);
 });
 
 test('navigating from home to fav pokemons clicking on two favorites', () => {
-  const { getByText, getByLabelText, getAllByText, container, history } = render(
+  const { getByText, getByLabelText, getAllByText } = render(
     <MemoryRouter>
       <App />
-    </MemoryRouter>
+    </MemoryRouter>,
   );
   localStorage.clear();
   fireEvent.click(getByText('Fire'));
   fireEvent.click(getByText('More details'));
   fireEvent.click(getByLabelText('Pokémon favoritado?'));
-  
+
   fireEvent.click(getByText('Home'));
   fireEvent.click(getByText('Bug'));
   fireEvent.click(getByText('More details'));
-    fireEvent.click(getByLabelText('Pokémon favoritado?'));
+  fireEvent.click(getByLabelText('Pokémon favoritado?'));
 
   fireEvent.click(getByText(/Favorite Pokémons/i));
   // expect(Pika).toBeInTheDocument();
