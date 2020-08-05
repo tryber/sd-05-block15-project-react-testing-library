fafc7c973d5cb2bfc5c049273367d88d80341ec1
RenataNovais <rnovaiscs@gmail.com>
2020-08-05 11:45:03 -0300

    2 more mutants down... requisito 3 - incompleto

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+
+test('no favorite page', () => {
+  const { getByText } = render(<FavoritePokemons />);
+
+  expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+});
