614232ffbbd5182ab11d6e240901c82b60aaaa3d
ferrari <luisdiasferrari@gmail.com>
2020-08-05 00:37:29 -0300

    requirement 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,30 +1,29 @@
-import React, { Component } from 'react';
-import { MemoryRouter, Router } from 'react-router-dom';
+import React from 'react';
+import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import { render, cleanup, fireEvent, getByAltText } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import About from '../components/About';
 
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
   return {
-  ...render(<Router history={history}>{component}</Router>), history,
+    ...render(<Router history={history}>{component}</Router>), history,
   };
-}; 
+};
 
 describe('A página "About" deve exibir informações sobre a Pokédex', () => {
   test('A página deve conter um heading h2 com o texto About Pokédex', () => {
     const { getByText } = renderWithRouter(<About />);
-      
+
     const h2Test = getByText(/About Pokédex/);
     expect(h2Test).toBeInTheDocument();
     expect(h2Test.tagName).toBe('H2');
-  
   });
 
   test('A página deve conter a seguinte imagem de uma Pokédex', () => {
     const { getByAltText } = renderWithRouter(<About />);
-    const url = `https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png`;
-    const img = getByAltText (/Pokédex/i);
+    const url = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const img = getByAltText(/Pokédex/i);
     expect(img.src).toBe(url);
-  })
+  });
 });
