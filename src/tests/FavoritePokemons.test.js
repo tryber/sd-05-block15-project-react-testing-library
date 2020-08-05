8c37edb28e621f4a21520517a13b1e50b1dc1bc1
Christian Duessel <duessel@gmail.com>
2020-08-05 10:54:55 -0300

    FavPoke

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,53 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { fireEvent, cleanup, render, getByRole, getByLabelText } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+afterEach(cleanup);
+
+test('renders `No favorite pokemon found`', () => {
+  const { getByText, container } = render(
+    <MemoryRouter>
+      <FavoritePokemons />
+    </MemoryRouter>,
+  );
+  const NoFav = getByText(`No favorite pokemon found`);
+  expect(NoFav).toBeInTheDocument();
+});
+
+test('navigating from home to fav pokemon clicking on one favorite', () => {
+  const { getByText, getByLabelText, getAllByText, container, history } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  fireEvent.click(getByText('More details'));
+  fireEvent.click(getByLabelText('Pokémon favoritado?'));
+  fireEvent.click(getByText(/Favorite Pokémons/i));
+  const Pika = getByText(`Pikachu`);
+  expect(Pika).toBeInTheDocument();
+  expect(getAllByText(/More details/i).length).toBe(1);
+});
+
+test('navigating from home to fav pokemons clicking on two favorites', () => {
+  const { getByText, getByLabelText, getAllByText, container, history } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  localStorage.clear();
+  fireEvent.click(getByText('Fire'));
+  fireEvent.click(getByText('More details'));
+  fireEvent.click(getByLabelText('Pokémon favoritado?'));
+  
+  fireEvent.click(getByText('Home'));
+  fireEvent.click(getByText('Bug'));
+  fireEvent.click(getByText('More details'));
+    fireEvent.click(getByLabelText('Pokémon favoritado?'));
+
+  fireEvent.click(getByText(/Favorite Pokémons/i));
+  // expect(Pika).toBeInTheDocument();
+  expect(getAllByText(/More details/i).length).toBe(2);
+});
