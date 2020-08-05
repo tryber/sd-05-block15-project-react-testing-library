6797fd1f83c7dbab25c5ef999b4460d08b06d52b
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-08-05 01:53:11 -0300

    tentando passar no requisito do SUMMARY

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -4,18 +4,18 @@ import App from '../App';
 import renderWithRouter from '../services/renderWithRouter';
 
 test('should render a Card of Pokémon', () => {
-  const { queryByText, getByRole, getByText, history, container } = renderWithRouter(<App />);
+  const { queryByText, queryAllByRole, getByText, history, container } = renderWithRouter(<App />);
   fireEvent.click(getByText(/More details/i));
   const moreDetails = (queryByText(/More details/i));
   const mapOfPokemon = (getByText(/Game Locations of Pikachu/i));
   const p = container.querySelector('p');
-  const h2 = container.querySelectorAll('h2')[1];
+  const summaryHeading = queryAllByRole('heading')[2];
   const pathName = history.location.pathname;
   const pokemonName = getByText(/Pikachu Details/i);
   expect(pathName).toBe('/pokemons/25');
   expect(pokemonName).toBeInTheDocument();
   expect(moreDetails).toBeNull();
-  expect(h2).toHaveTextContent('Summary');
+  expect(summaryHeading).toHaveTextContent(/Summary/i);
   expect(p).toBeInTheDocument();
   expect(mapOfPokemon).toBeInTheDocument();
 });
