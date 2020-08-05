4d37fa7dacc68276200ccc99979df9646cf74b10
Rafael Assad <rafaassadeng@gmail.com>
2020-08-05 10:28:45 -0300

    github bug

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderWithRouter from '../renderWithRouter';
 import About from '../components/About';
+// blablabla
 
 test('Testes do arquivo About.js', () => {
   const { getByText, container } = renderWithRouter(<About />);
