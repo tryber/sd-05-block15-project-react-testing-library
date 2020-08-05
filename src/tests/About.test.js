89d1ba13d6edd5aff90e4cb53b3495d522e5f98d
Rafael Assad <rafaassadeng@gmail.com>
2020-08-05 14:40:22 -0300

    mmmm

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import renderWithRouter from '../renderWithRouter';
 import About from '../components/About';
-// blablablaa
 
-test('Testes do arquivo About.js', () => {
+test('A página "About" deve exibir informações sobre a Pokédex', () => {
   const { getByText, container } = renderWithRouter(<About />);
   expect(getByText(/About Pokédex/i)).toBeInTheDocument();
   expect(container.querySelector('h2')).toBeInTheDocument();
+  expect(container.querySelector('img').src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   expect(container.querySelectorAll('p').length).toEqual(2);
-  expect(container.querySelector('img')).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
 });
