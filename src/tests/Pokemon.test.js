8d30a76aee1dded8cfd8e899db51c0d3b073fd67
pedrohcalado <pedrocalado22@gmail.com>
2020-08-05 12:21:44 -0300

    Tests on Pokemon.test.js

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('Deve ser retornado um card com as informações de determinado pokémon', () => {
+  const { getByLabelText, getByText, getByTestId, container, history } = renderWithRouter(<App />);
+  expect(getByTestId('pokemon-name').innerHTML).toBe('Pikachu');
+  expect(getByTestId('pokemonType').innerHTML).toBe('Electric');
+  expect(getByTestId('pokemon-weight').innerHTML).toBe('Average weight:6.0kg');
+  fireEvent.click(getByText('More details'));
+  const pathname = history.location.pathname;
+  expect(pathname).toBe('/pokemons/25');
+  expect(container.querySelector('img').alt).toBe('Pikachu sprite');
+  expect(container.querySelector('img').src).toBe('https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  fireEvent.click(getByLabelText('Pokémon favoritado?'));
+  fireEvent.click(getByText('Favorite Pokémons'));
+  expect(container.querySelectorAll('img')[1].src).toBe('http://localhost/star-icon.svg');
+  expect(container.querySelectorAll('img')[1].alt).toBe('Pikachu is marked as favorite');
+});
