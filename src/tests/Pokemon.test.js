eb8a92aa4f9a0f2478635990ee10ca62ec66bb56
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-08-05 17:48:07 -0300

    kkkkkk

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -29,7 +29,7 @@ describe('Deve ser retornado um card com as informações de determinado pokémo
     );
   });
 
-  test('A imagem deve conter atributos src e alt', () => {
+  test('A imagem deve conter atributos src e alt e A imagem deve conter atributos src e alt', () => {
     const { getByRole, getByAltText } = render(
       <MemoryRouter>
         <App />
@@ -41,14 +41,6 @@ describe('Deve ser retornado um card com as informações de determinado pokémo
       'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
     );
     expect(getByAltText('Pikachu sprite')).toBeInTheDocument();
-  });
-
-  test('A imagem deve conter atributos src e alt', () => {
-    const { getByRole, getByAltText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
 
     expect(getByRole('img')).toHaveAttribute(
       'src',
@@ -56,6 +48,20 @@ describe('Deve ser retornado um card com as informações de determinado pokémo
     );
     expect(getByAltText('Pikachu sprite')).toBeInTheDocument();
   });
+
+  // test('A imagem deve conter atributos src e alt', () => {
+  //   // const { getByRole, getByAltText } = render(
+  //   //   <MemoryRouter>
+  //   //     <App />
+  //   //   </MemoryRouter>,
+  //   // );
+
+  //   expect(getByRole('img')).toHaveAttribute(
+  //     'src',
+  //     'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+  //   );
+  //   expect(getByAltText('Pikachu sprite')).toBeInTheDocument();
+  // });
 
   test('O pokémon exibido na Pokédex deve conter um link de navegação', () => {
     const { getByText } = render(
