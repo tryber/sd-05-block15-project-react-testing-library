23816595ed9bfccfdd93464ee5ffc0c061876ae7
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-08-05 01:30:16 -0300

    alterando const h2

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -4,18 +4,18 @@ import App from '../App';
 import renderWithRouter from '../services/renderWithRouter';
 
 test('should render a Card of Pokémon', () => {
-  const { queryByText, getByText, history, container } = renderWithRouter(<App />);
+  const { queryByText, getByRole, getByText, history, container } = renderWithRouter(<App />);
   fireEvent.click(getByText(/More details/i));
   const moreDetails = (queryByText(/More details/i));
   const mapOfPokemon = (getByText(/Game Locations of Pikachu/i));
   const p = container.querySelector('p');
-  const summary = (queryByText(/Summary/i));
+  const h2 = container.querySelectorAll('h2')[1];
   const pathName = history.location.pathname;
   const pokemonName = getByText(/Pikachu Details/i);
   expect(pathName).toBe('/pokemons/25');
   expect(pokemonName).toBeInTheDocument();
   expect(moreDetails).toBeNull();
-  expect(summary).toBeInTheDocument();
+  expect(h2).toHaveTextContent('Summary');
   expect(p).toBeInTheDocument();
   expect(mapOfPokemon).toBeInTheDocument();
 });
