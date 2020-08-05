a5cdbb4a74427811cbb8f49c9429aa7763a6fc8d
Mau <mauricio.sdjr@gmail.com>
2020-08-05 10:53:23 -0300

    About.test.js refatorado

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,11 +4,24 @@ import About from '../components/About';
 
 afterEach(cleanup);
 
-test('Elementos no componente About', () => {
-  const { queryByText, container } = render(<About />);
-  const aboutTag = queryByText('About Pokédex');
-  expect(aboutTag).toBeInTheDocument();
-  expect(aboutTag.tagName).toBe('H2');
+test('A página "About" deve exibir informações sobre a Pokédex', () => {
+  const { queryByText } = render(<About />);
+  expect(queryByText(/About Pokédex/i)).toBeInTheDocument();
+});
+
+test('A página deve conter um heading h2 com o texto About Pokédex', () => {
+  const { queryByText, getByRole } = render(<About />);
+  expect(getByRole('heading')).toBeInTheDocument();
+  expect(queryByText('About Pokédex').tagName).toBe('H2');
+});
+
+test('A página deve conter dois parágrafos com texto sobre a Pokédex', () => {
+  const { container } = render(<About />);
   expect(container.querySelectorAll('p').length).toBe(2);
-  expect(container.querySelector('img')).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
+
+test('A página deve conter a referida imagem de uma Pokédex', () => {
+  const { getByRole } = render(<About />);
+  expect(getByRole('img')).toBeInTheDocument();
+  expect(getByRole('img').src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+afterEach(cleanup);
+
+test('Elementos no componente NotFound', () => {
+  const { queryByText, container } = render(<NotFound />);
+  const notFound = queryByText('Page requested not found 😭');
+  expect(notFound.tagName).toBe('H2');
+  expect(container.querySelector('img')).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
