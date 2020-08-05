b3b11069f52c94a6026cd74281f0983b619e6fce
Rafael Assad <rafaassadeng@gmail.com>
2020-08-05 09:45:26 -0300

    not found

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { cleanup } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('notFound', () => {
+  afterEach(cleanup);
+  test('A página deve conter um heading h2 com o texto Page requested not found 😭', () => {
+    renderWithRouter(<NotFound />);
+    const headh2 = document.querySelector('h2');
+    expect(headh2).toBeInTheDocument();
+    expect(headh2).toHaveTextContent('Page requested not found');
+  });
+
+  test('A página deve exibir a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const pikaPika = getByRole('img');
+    expect(pikaPika[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
