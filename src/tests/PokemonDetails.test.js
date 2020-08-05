c06563fc4384405aa5ac96015aa96f89926bb187
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-08-05 11:21:53 -0300

    PokemonDetails.Test.js Done

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -4,18 +4,28 @@ import App from '../App';
 import renderWithRouter from '../services/renderWithRouter';
 
 test('should render a Card of Pokémon', () => {
-  const { queryByText, queryAllByRole, getByText, history, container } = renderWithRouter(<App />);
+  const { queryByText, queryAllByRole, getAllByAltText, getByLabelText, getByText, history, container } = renderWithRouter(<App />);
   fireEvent.click(getByText(/More details/i));
   const moreDetails = (queryByText(/More details/i));
   const mapOfPokemon = (getByText(/Game Locations of Pikachu/i));
-  const p = container.querySelector('p');
+  const p = container.querySelectorAll('p')[3];
   const summaryHeading = queryAllByRole('heading')[2];
   const pathName = history.location.pathname;
   const pokemonName = getByText(/Pikachu Details/i);
+  const labelText = getByLabelText(/Pokémon favoritado?/i);
+  const mapImg1 = getAllByAltText(/Pikachu Location/i)[0];
+  const mapImg2 = getAllByAltText(/Pikachu Location/i)[1];
+
   expect(pathName).toBe('/pokemons/25');
   expect(pokemonName).toBeInTheDocument();
   expect(moreDetails).toBeNull();
-  expect(summaryHeading).toHaveTextContent(/Summary/i);
+  expect(p).toHaveTextContent('This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.');
   expect(p).toBeInTheDocument();
+  expect(summaryHeading).toHaveTextContent('Summary');
   expect(mapOfPokemon).toBeInTheDocument();
+  expect(labelText).toBeInTheDocument();
+  expect(mapImg1).toBeInTheDocument();
+  expect(mapImg2).toBeInTheDocument();
+  expect(mapImg1.src).toBe('https://cdn.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+  expect(mapImg2.src).toBe('https://cdn.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
 });
