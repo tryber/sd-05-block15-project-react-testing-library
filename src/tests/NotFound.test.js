f2874a22e1b6b0464c4bc88a9f359dfa5e435f1c
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-08-05 18:09:06 -0300

    CC

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,28 +1,18 @@
 import React from 'react';
-import { createMemoryHistory } from 'history';
-import { NotFound } from '../components';
-import { Router } from 'react-router-dom';
 import { render, cleanup } from '@testing-library/react';
-
-function renderWithRouter(ui, { route = '/', history = createMemoryHistory(
-  { initialEntries: [route] }) } = {}) {
-    return {
-      ...render(<Router history={history}>{ui}</Router>),
-      history,
-    };
-  }
+import { NotFound } from '../components';
 
 describe('Teste da página NotFound', () => {
   afterEach(cleanup);
 
   test('A página deve conter um heading h2 com o texto Page requested not found 😭', () => {
-    const { getByRole } = renderWithRouter(<NotFound />);
+    const { getByRole } = render(<NotFound />);
     const h2 = getByRole('heading');
-    expect(h2).toHaveTextContent('Page requested not found 😭')
+    expect(h2).toHaveTextContent('Page requested not found 😭');
   });
 
   test('A página deve exibir a imagem', () => {
-    const { getAllByRole } = renderWithRouter(<NotFound />);
+    const { getAllByRole } = render(<NotFound />);
     const image = getAllByRole('img');
     expect(image[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
   });
