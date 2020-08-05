d1ccfe4097744c3123b3aca88529c0a0e17a457c
Virginia Duca <virginiambd.bio@gmail.com>
2020-08-05 18:45:05 -0300

    codeclimate

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -7,7 +7,7 @@ import App from '../App';
 
 afterEach(cleanup);
 
-const renderWithRouter = (component) => {
+const renderWithRouterr = (component) => {
   const history = createMemoryHistory();
   return {
     ...render(<Router history={history}>{component}</Router>), history,
@@ -15,21 +15,21 @@ const renderWithRouter = (component) => {
 };
 
 test('link about should render about page', () => {
-  const { getByText, history } = renderWithRouter(<App />);
+  const { getByText, history } = renderWithRouterr(<App />);
   fireEvent.click(getByText(/about/i));
   const pathname = history.location.pathname;
   expect(pathname).toBe('/about');
 });
 
 test('about page should have heading About pokedex', () => {
-  const { getByRole } = renderWithRouter(<About />);
+  const { getByRole } = renderWithRouterr(<About />);
   const h2 = getByRole('heading');
   expect(h2).toBeInTheDocument();
   expect(h2.innerHTML).toBe('About Pokédex');
 });
 
 test('about page should have two paragraphs', () => {
-  const { getByText } = renderWithRouter(<About />);
+  const { getByText } = renderWithRouterr(<About />);
   const p1 = getByText(/This application simulates a Pokédex, a digital encliclopedia containing all Pokémons/i);
   const p2 = getByText(/One can filter Pokémons by type, and see more details for each one of them/i);
   const allps = [p1, p2];
@@ -39,7 +39,7 @@ test('about page should have two paragraphs', () => {
 });
 
 test('about page should have pokedex image', () => {
-  const { getByRole } = renderWithRouter(<About />);
+  const { getByRole } = renderWithRouterr(<About />);
   const img = getByRole('img');
   expect(img.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
 });
