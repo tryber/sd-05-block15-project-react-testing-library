93b6f90a77823c9a32d03b2820dbe15835852999
RenataNovais <rnovaiscs@gmail.com>
2020-08-05 12:22:54 -0300

    terceiro requisito finalizado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { MemoryRouter, Router } from 'react-router-dom';
 import { FavoritePokemons } from '../components';
+import App from '../App';
 
-test('no favorite page', () => {
+test('no favorite pokemons page', () => {
   const { getByText } = render(<FavoritePokemons />);
 
   expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+});
+
+test('it renders all favorite cards', () => {
+  const history = createMemoryHistory();
+  const { getByText, getByLabelText } = render(
+    <Router history={history}>
+      <App />
+    </Router>,
+  );
+
+  const moreDetails = getByText(/more details/i);
+  fireEvent.click(moreDetails);
+  const labelFavorite = getByLabelText('Pokémon favoritado?');
+  fireEvent.click(labelFavorite);
+  const favPokemonsPage = getByText('Favorite Pokémons');
+  fireEvent.click(favPokemonsPage);
+
+  expect(getByText(/more details/i)).toBeInTheDocument();
 });
