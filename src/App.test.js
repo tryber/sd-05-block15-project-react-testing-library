2e666c13ae98d8a2f9bc2977271fd0a08c8addf3
luialbeto <araujo.script@gmail.com>
2020-08-05 16:48:43 -0300

    requisito1-free

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,15 @@
-test('should ', () => {
-  
-})
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
 
+
+test('shows the Pokédex when the route is `/`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
