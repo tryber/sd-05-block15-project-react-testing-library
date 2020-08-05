e064e33582602e6a49074c69b9b7f907eaca63fb
RenataNovais <rnovaiscs@gmail.com>
2020-08-05 16:46:54 -0300

    primeiro bullet - requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+test('Ao apertar o botão de próximo, a página deve exibir o próximo pokémon da lista', () => {
+  const history = createMemoryHistory();
+  const { getByText } = render(
+    <Router history={history}>
+      <App />
+    </Router>,
+  );
+  const pokemons = ['Charmander', 'Caterpie', 'Ekans', 'Alakazam', 'Mew', 'Rapidash', 'Snorlax', 'Dragonair', 'Pikachu'];
+
+  const nextButton = getByText('Próximo pokémon');
+
+  pokemons.forEach((poke) => {
+    fireEvent.click(nextButton);
+    expect(getByText(poke)).toBeInTheDocument();
+  });
+});
