8741a0545e236f94a4274c902d16e118bdbe4d98
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-08-05 15:58:20 -0300

    pokemon test component done

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import App from '../App';
+
+test('Testing pokemon component', () => {
+  const { getByText, getAllByText, getAllByTestId } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  )
+  const all = getByText('All');
+  fireEvent.click(all);
+  expect(getAllByText('Electric').length).toBe(2);
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  expect(getByText('Próximo pokémon')).toBeInTheDocument();
+  expect(getAllByTestId('pokemon-type-button').length).toBe(7);
+});
