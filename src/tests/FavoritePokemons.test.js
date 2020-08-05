7c409fc5e2bcd9447d8295820302d6991d42c3ca
luiz-avelar <luiz.avelar25@gmail.com>
2020-08-05 18:34:09 -0300

    Requirements completed

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { cleanup, fireEvent, render } from '@testing-library/react';
 import FavoritePokemons from '../components/FavoritePokemons';
-import App from '../App';
 import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
 
 afterEach(cleanup);
 
-describe('', () => {
-
-  test('', () => {
+describe('Testes do arquivo FavoritePokemons.js', () => {
+  test('Caso a pessoa não tenha pokémons favoritos, a mensagem No favorite pokemon found deve aparecer na tela.', () => {
     const { getByText } = render(
       <MemoryRouter>
         <FavoritePokemons />
@@ -18,23 +17,28 @@ describe('', () => {
     expect(getByText('No favorite pokemon found')).toBeInTheDocument();
   });
 
-  test('', () => {
-    const { getByText, getByLabelText } = render(<MemoryRouter><App /></MemoryRouter>);
+  test('A página não deve exibir nenhum card de pokémon não favoritado', () => {
+    const { getByText, getByLabelText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>);
 
     fireEvent.click(getByText(/more details/i));
     fireEvent.click(getByLabelText(/pokémon favoritado/i));
     fireEvent.click(getByText(/Favorite Pokémons/i));
 
     expect(getByText(/More details/i)).toBeInTheDocument();
+  });
 
-  })
-
-  test('', () => {
-    const { getByText, getByLabelText, getAllByText } = render(<MemoryRouter><App /></MemoryRouter>);
+  test('A página deve exibir todos os cards de pokémons favoritados', () => {
+    const { getByText, getByLabelText, getAllByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>);
 
     localStorage.clear();
 
-    fireEvent.click(getByText(/bug/i))
+    fireEvent.click(getByText(/bug/i));
     fireEvent.click(getByText(/more details/i));
     fireEvent.click(getByLabelText(/pokémon favoritado/i));
     fireEvent.click(getByText(/home/i));
@@ -44,15 +48,6 @@ describe('', () => {
     fireEvent.click(getByLabelText(/pokémon favoritado/i));
     fireEvent.click(getByText(/Favorite Pokémons/i));
 
-    expect(getAllByText(/More details/i).length).toBe(2);
-
-  })
-
-})
-
-
-// Caso a pessoa não tenha pokémons favoritos, a mensagem No favorite pokemon found deve aparecer na tela. OK
-
-// A página não deve exibir nenhum card de pokémon não favoritado. OK
-
-// A página deve exibir todos os cards de pokémons favoritados; OK
+    expect(getAllByText(/More details/i)).toHaveLength(2);
+  });
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,108 +1,88 @@
 import React from 'react';
-import { cleanup, render, fireEvent, queryByText } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
-import { Router } from 'react-router-dom';
-import App from '../App';
+import { cleanup, render, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import { pokemonType } from '../types';
-
+import App from '../App';
 
 afterEach(cleanup);
 
-test('O botão deve conter o texto Próximo pokémon', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+describe('Testes do arquivo Pokedex.js', () => {
+  test('O botão deve conter o texto Próximo pokémon', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
 
-  expect(getByText(/próximo pokémon/i)).toBeInTheDocument();
-  expect(getByText(/próximo pokémon/i).tagName).toBe('BUTTON');
-});
+    expect(getByText(/próximo pokémon/i)).toBeInTheDocument();
+    expect(getByText(/próximo pokémon/i).tagName).toBe('BUTTON');
+  });
 
-test('Cliques sucessivos no botão devem mostrar o próximo pokémon da lista', () => {
-  const { getByText, getAllByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  test('Página home deve ter o texto "Encountered pokémons"', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
 
-  const pokemonsNames = ['Charmander', 'Caterpie', 'Ekans', 'Alakazam', 'Mew', 'Rapidash', 'Snorlax', 'Dragonair', 'Pikachu'];
+    expect(getByText(/encountered pokémons/i)).toBeInTheDocument();
+  });
 
-  for (let i = 0; i < pokemonsNames.length; i += 1) {
-    fireEvent.click(getByText(/próximo pokémon/i));
-    expect(getByText(pokemonsNames[i])).toBeInTheDocument();
-    expect(getAllByText(/Average weight/i)).toHaveLength(1);
-  }
-})
+  test('A Pokédex deve conter botões de filtro', () => {
+    const { getAllByTestId } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
 
-test('A Pokédex deve conter botões de filtro', () => {
-  const { getAllByTestId } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+    expect(getAllByTestId('pokemon-type-button')).toHaveLength(7);
+  });
 
-  expect(getAllByTestId('pokemon-type-button')).toHaveLength(7);
-})
+  test('A partir da seleção de um botão de tipo, a Pokédex deve circular somente pelos pokémons daquele tipo', () => {
+    const { getByText, getByTestId, getAllByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
 
-test('A partir da seleção de um botão de tipo, a Pokédex deve circular somente pelos pokémons daquele tipo', () => {
-  const { getByText, getByTestId } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-
-  const pokemonTypes = ['Eletric', 'Fire', 'Bug', 'Poison', 'Psych'];
-
-  for (let i = 0; i < pokemonTypes.length; i += 1) {
-    fireEvent.click(getByText(pokemonType[i]));
+    const pokemonTypes = ['Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+    fireEvent.click(getAllByText(pokemonTypes[0])[1]);
     expect(getByTestId('pokemonType')).toBeInTheDocument();
-    expect(getByTestId('pokemonType')).toBe(pokemonType[i]);
-  }
-})
+    expect(getByTestId('pokemonType').innerHTML).toBe(pokemonTypes[0]);
 
-test('', () => {
-  const { getByText, getAllByText, getAllByTestId } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+    for (let i = 1; i < pokemonTypes.length; i += 1) {
+      fireEvent.click(getByText(pokemonTypes[i]));
+      expect(getByTestId('pokemonType')).toBeInTheDocument();
+      expect(getByTestId('pokemonType').innerHTML).toBe(pokemonTypes[i]);
+    }
+  });
 
-  // expect(getAllByTestId('pokemon-type-button')).toHaveLength(7);
-  
-  fireEvent.click(getByText(/Psych/i));
-  expect(getByText('Alakazam')).toBeInTheDocument();
-  
-});
+  test('A Pokédex deve conter um botão para resetar o filtro', () => {
+    const { getByText, getAllByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
 
-test('A Pokédex deve conter um botão para resetar o filtro', () => {
-  const { getByText, getAllByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+    expect(getByText(/all/i)).toBeInTheDocument();
+    fireEvent.click(getByText(/all/i));
 
-  expect(getByText(/all/i)).toBeInTheDocument();
-  fireEvent.click(getByText(/all/i));
-    
-  const pokemonsNames = ['Charmander', 'Caterpie', 'Ekans', 'Alakazam', 'Mew', 'Rapidash', 'Snorlax', 'Dragonair', 'Pikachu'];
+    const pokemonsNames = ['Charmander', 'Caterpie', 'Ekans', 'Alakazam', 'Mew', 'Rapidash', 'Snorlax', 'Dragonair', 'Pikachu'];
 
-  for (let i = 0; i < pokemonsNames.length; i += 1) {
-    fireEvent.click(getByText(/próximo pokémon/i));
-    expect(getByText(pokemonsNames[i])).toBeInTheDocument();
-    expect(getAllByText(/Average weight/i)).toHaveLength(1);
-  }
-});
+    for (let i = 0; i < pokemonsNames.length; i += 1) {
+      fireEvent.click(getByText(/próximo pokémon/i));
+      expect(getByText(pokemonsNames[i])).toBeInTheDocument();
+      expect(getAllByText(/Average weight/i)).toHaveLength(1);
+    }
+  });
 
-test('O botão de Próximo pokémon deve ser desabilitado se a lista filtrada de pokémons tiver um só pokémon', () => {
-  const { getByText, getAllByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  test('O botão de Próximo pokémon deve ser desabilitado se a lista filtrada de pokémons tiver um só pokémon', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
 
-  fireEvent.click(getByText(/Bug/i));
-  expect(getByText(/próximo pokémon/i)).toBeDisabled();
-
+    fireEvent.click(getByText(/Bug/i));
+    expect(getByText(/próximo pokémon/i)).toBeDisabled();
+  });
 });
