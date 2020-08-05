bcc8f1af7477be2374db4a7e791ba949694c7812
César Alves <alvescesar@hotmail.com>
2020-08-04 21:51:00 -0300

    Requisite 1, links checked

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,22 +3,56 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Ao carregar a aplicação no caminho de URL “/”, a página principal da Pokédex deve ser mostrada', () => {
+  it('renderiza um header com o texto `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const header = getByText(/Pokédex/i);
+    expect(header).toBeInTheDocument();
+  });
+
+  it('mostra a Pokédex quando a rota é `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
 });
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
+describe('No topo da aplicação, deve haver um conjunto fixo de links de navegação', () => {
+  it('O primeiro link deve possuir o texto Home com a URL /', () => {
+    const { getAllByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(getAllByRole('link')[0]).toHaveAttribute('href', '/');
+    expect(getAllByRole('link')[0].innerHTML).toMatch(/home/i);
+  });
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  it('O segundo link deve possuir o texto About com a URL /about', () => {
+    const { getAllByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(getAllByRole('link')[1]).toHaveAttribute('href', '/about');
+    expect(getAllByRole('link')[1].innerHTML).toMatch(/about/i);
+  });
+
+  it('O terceiro link deve possuir o texto Favorite Pokémons com a URL /favorites', () => {
+    const { getAllByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(getAllByRole('link')[2]).toHaveAttribute('href', '/favorites');
+    expect(getAllByRole('link')[2].innerHTML).toMatch(/Favorite Pokémons/i);
+  });
 });
