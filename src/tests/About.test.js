445327fa9f3cc71c2c631ee916cd57d3662b75d9
Rafael Assad <rafaassadeng@gmail.com>
2020-08-05 10:22:52 -0300

    commas do capeta

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -7,7 +7,5 @@ test('Testes do arquivo About.js', () => {
   expect(getByText(/About Pokédex/i)).toBeInTheDocument();
   expect(container.getElementsByTagName('h2')).toBeInTheDocument();
   expect(container.querySelectorAll('p').length).toEqual(2);
-  expect(container.getElementsByTagName('img')).toBe(
-    'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
-  );
+  expect(container.getElementsByTagName('img')).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
 });
