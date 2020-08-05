79d06a7e035f38055119649ffa67ba9e23398edc
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-08-05 01:08:34 -0300

    verificando quais requisitos ainda faltam

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+
+test('should render a Card of Pokémon', () => {
+  const { queryByText, getByText, history, container } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/More details/i));
+  const moreDetails = (queryByText(/More details/i));
+  const mapOfPokemon = (getByText(/Game Locations of Pikachu/i));
+  const p = container.querySelector('p');
+  const summary = (getByText(/Summary/i));
+  const pathName = history.location.pathname;
+  const pokemonName = getByText(/Pikachu Details/i);
+  expect(pathName).toBe('/pokemons/25');
+  expect(pokemonName).toBeInTheDocument();
+  expect(moreDetails).toBeNull();
+  expect(summary).toBeInTheDocument();
+  expect(p).toBeInTheDocument();
+  expect(mapOfPokemon).toBeInTheDocument();
+});
