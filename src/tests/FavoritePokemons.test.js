5050ba7dc818c898d8c0787bf514b509338513ef
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-08-06 08:31:24 -0300

    CodeClimate

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import FavoritePokemons from '../components/FavoritePokemons';
-import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
 
 test('Caso a pessoa não tenha pokémons favoritos, a mensagem No favorite pokemon found deve aparecer na tela', () => {
   const { getByText } = render(
