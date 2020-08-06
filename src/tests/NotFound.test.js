64822bd3b88b105748f25787bea21a91fb8aac35
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-08-06 10:11:44 -0300

    Not Found testing

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+test('A página deve conter um heading h2 com o texto Page requested not found', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <NotFound />
+    </MemoryRouter>
+  );
+  const h2 = getByText(/Page requested not found/i);
+  expect(h2).toBeInTheDocument();
+  expect(h2.tagName).toBe('H2');
+});
+
+test('A página deve exibir a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+  const { container } = render(
+    <MemoryRouter>
+      <NotFound />
+    </MemoryRouter>
+  );
+  const image = container.querySelector('img');
+  expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
