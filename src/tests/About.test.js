08f5c97fcbdaef557acff8edbd90de01518d9548
Marylange <marylangess@gmail.com>
2020-08-08 13:52:12 -0300

    verificando CC

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -5,40 +5,40 @@ import { render, fireEvent } from '@testing-library/react';
 import About from '../components/About';
 
 const renderWithRouter = (component) => {
-    const history = createMemoryHistory();
-    return {
-        ...render(<MemoryRouter history={history}>{component}</MemoryRouter>),
-        history,
-    };
+  const history = createMemoryHistory();
+  return {
+    ...render(<MemoryRouter history={history}>{component}</MemoryRouter>),
+    history,
+  };
 };
 
 describe('2. Testes do arquivo About.js', () => {
 
-    test('2.1 - A página "About" deve exibir informações sobre a Pokédex', () => {
-        const { getByTestId } = renderWithRouter(<About />);
+  test('2.1 - A página "About" deve exibir informações sobre a Pokédex', () => {
+    const { getByTestId } = renderWithRouter(<About />);
 
-        const infoPokedex = getByTestId('infos');
-        expect(infoPokedex).toBeInTheDocument();
-    });
+    const infoPokedex = getByTestId('infos');
+    expect(infoPokedex).toBeInTheDocument();
+  });
 
-    test('2.2 - A página deve conter um heading h2 com o texto About Pokédex', () => {
-        const { getByText } = renderWithRouter(<About />);
+  test('2.2 - A página deve conter um heading h2 com o texto About Pokédex', () => {
+    const { getByText } = renderWithRouter(<About />);
 
-        const veriTitulo = getByText(`About ` + `Pokédex`);
-        expect(veriTitulo).toBeInTheDocument();
-    });
+    const veriTitulo = getByText(`About ` + `Pokédex`);
+    expect(veriTitulo).toBeInTheDocument();
+  });
 
-    test('2.3 - A página deve conter dois parágrafos com texto sobre a Pokédex', async () => {
-        const { querySelectorAll } = renderWithRouter(<About />);
+  test('2.3 - A página deve conter dois parágrafos com texto sobre a Pokédex', async () => {
+    const { querySelectorAll } = renderWithRouter(<About />);
 
-        const veriTagP = container.querySelectorAll('p');
-        expect(veriTagP).toBeInTheDocument(2);
-    });
+    const veriTagP = container.querySelectorAll('p');
+    expect(veriTagP).toBeInTheDocument(2);
+  });
 
-    test('2.4 - A página deve conter a seguinte imagem de uma Pokédex', () => {
-        const { getByAltText } = renderWithRouter(<About />);
+  test('2.4 - A página deve conter a seguinte imagem de uma Pokédex', () => {
+    const { getByAltText } = renderWithRouter(<About />);
 
-        const altImg = getByAltText('Pokédex');
-        expect(altImg).toBeInTheDocument();
-    });
+    const altImg = getByAltText('Pokédex');
+    expect(altImg).toBeInTheDocument();
+  });
 });
