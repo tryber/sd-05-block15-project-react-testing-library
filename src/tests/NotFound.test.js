f9f6af4f97d941836eae91794ff55f441833ebfc
Fas-fCode <fasnaweb2020@gmail.com>
2020-08-09 03:14:26 -0300

    requisito ok

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { render, cleanup, getByText } from '@testing-library/react';
+import Notfound from '../components/NotFound';
+afterEach(cleanup);
+
+test('A página deve conter um heading h2 com o texto Page requested not found ', () => {
+  const { getByText } = render(<Notfound />);
+  const texto = getByText('Page requested not found');
+  expect(texto).toBeInTheDocument();
+  expect(document.querySelector('h2')).toBeInTheDocument();
+  expect(texto.tagName).toBe('H2');
+});
+
+test('A página deve exibir a imagem  ', () => {
+  const { getAllByRole } = render(<Notfound />);
+  const imagem = getAllByRole('img')[1];
+  expect(imagem).toBeInTheDocument();
+  expect(imagem.src).toBe(
+    'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+  );
+});
