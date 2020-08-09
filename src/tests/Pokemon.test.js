95d8073edee3e682a118a2fcc78ce3601e77d461
ferrari <luisdiasferrari@gmail.com>
2020-08-09 19:03:04 -0300

    requeriment 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -57,30 +57,20 @@ describe('Deve ser retornado um card com as informações de determinado pokémo
     expect(imagem.alt).toMatch('sprite');
   });
 
-  test('testa a imagem do pokemon', () => {
-    const { getAllByRole } = renderWithRouter(<App />);
-    const pokemonImage = getAllByRole('img')[0];
-    expect(pokemonImage).toBeInTheDocument();
-    expect(pokemonImage.src).toMatch('https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
-    expect(pokemonImage.alt).toMatch(/sprite/);
-  });
-
   test('link para mais detalhes', () => {
     const { getAllByRole, history } = renderWithRouter(<App />);
-    const buttonDetails = getAllByRole('link')[3];
-    expect(buttonDetails).toBeInTheDocument();
-    const linkPath = buttonDetails.pathname;
-    expect(buttonDetails.href).toMatch('/pokemons/25');
-    fireEvent.click(buttonDetails);
-    expect(history.location.pathname).toMatch(linkPath);
+    const detalhesButton = getAllByRole('link')[3];
+    console.log(detalhesButton);
+    expect(detalhesButton).toBeInTheDocument();
+    expect(detalhesButton.href).toMatch('/pokemons/25');
   });
 
   test('pokemons favoritos possuem marcação', () => {
     const { getAllByRole } = renderWithRouter(
       <Pokemon pokemon={pokemons[0]} isFavorite />,
     );
-    const imgFavorito = getAllByRole('img')[1];
-    expect(imgFavorito.src).toMatch(/\/star-icon.svg/);
-    expect(imgFavorito.alt).toMatch('Pikachu is marked as favorite');
+    const imgagemFavorito = getAllByRole('img')[1];
+    expect(imgagemFavorito.src).toMatch(/\/star-icon.svg/);
+    expect(imgagemFavorito.alt).toMatch('Pikachu is marked as favorite');
   });
 });
