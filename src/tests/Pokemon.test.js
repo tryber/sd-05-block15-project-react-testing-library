32b79aa528b0f924cb1be83f62a6afbd2693b0c1
ferrari <luisdiasferrari@gmail.com>
2020-08-09 16:29:00 -0300

    requeriment 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import App from '../App';
+import Pokemon from '../components/Pokemon';
+import pokemons from '../data';
 
 const renderWithRouter = (component) => {
-  const historyNot = createMemoryHistory();
+  const historyPokemon = createMemoryHistory();
   return {
-    ...render(<Router history={historyNot}>{component}</Router>), history,
+    ...render(<Router history={historyPokemon}>{component}</Router>), history,
   };
 };
 
@@ -41,7 +43,7 @@ describe('Deve ser retornado um card com as informações de determinado pokémo
 
     const weight = getByTestId(/pokemon-weight/);
     expect(weight).toBeInTheDocument();
-    expect(weight.innerHTML).toMatch('Average weight:6.0kg'); 
+    expect(weight.innerHTML).toMatch('Average weight:6.0kg');
   });
 
   test('A imagem deve conter um atributo src e alt', () => {
@@ -50,8 +52,35 @@ describe('Deve ser retornado um card com as informações de determinado pokémo
     const imagem = getAllByRole(/img/)[0];
     expect(imagem).toBeInTheDocument();
     expect(imagem).toHaveAttribute('src');
-    expect(imagem.src).toMatch('https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png'); 
+    expect(imagem.src).toMatch('https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
     expect(imagem).toHaveAttribute('alt');
     expect(imagem.alt).toMatch('sprite');
+  });
+
+  test('testa a imagem do pokemon', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+    const pokemonImage = getAllByRole('img')[0];
+    expect(pokemonImage).toBeInTheDocument();
+    expect(pokemonImage.src).toMatch('https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    expect(pokemonImage.alt).toMatch(/sprite/);
+  });
+
+  test('link para mais detalhes', () => {
+    const { getAllByRole, history } = renderWithRouter(<App />);
+    const buttonDetails = getAllByRole('link')[3];
+    expect(buttonDetails).toBeInTheDocument();
+    const linkPath = buttonDetails.pathname;
+    expect(buttonDetails.href).toMatch('/pokemons/25');
+    fireEvent.click(buttonDetails);
+    expect(history.location.pathname).toMatch(linkPath);
+  });
+
+  test('pokemons favoritos possuem marcação', () => {
+    const { getAllByRole } = renderWithRouter(
+      <Pokemon pokemon={pokemons[0]} isFavorite />,
+    );
+    const imgFavorito = getAllByRole('img')[1];
+    expect(imgFavorito.src).toMatch(/\/star-icon.svg/);
+    expect(imgFavorito.alt).toMatch('Pikachu is marked as favorite');
   });
 });
