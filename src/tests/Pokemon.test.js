857400c986205f928e3a1605b1a01ad7f627a70e
ferrari <luisdiasferrari@gmail.com>
2020-08-09 13:19:04 -0300

    requeriment 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,9 +1,8 @@
-test('', () => {});
-/* import React from 'react';
+import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import { render } from '@testing-library/react';
-import Pokemon from '../components/Pokemon';
+import { render, cleanup } from '@testing-library/react';
+import App from '../App';
 
 const renderWithRouter = (component) => {
   const historyNot = createMemoryHistory();
@@ -15,10 +14,32 @@ const renderWithRouter = (component) => {
 describe('Deve ser retornado um card com as informações de determinado pokémon', () => {
   afterEach(cleanup);
 
+  test('a página possui um heading Encountered Pokemons', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const paginaRender = getByText('Encountered pokémons');
+    expect(paginaRender).toBeInTheDocument();
+  });
+
   test('O nome correto do pokémon deve aparecer na tela', () => {
-    const { getByTestId } = renderWithRouter(<Pokemon />);
+    const { getByTestId } = renderWithRouter(<App />);
 
     const nome = getByTestId(/pokemon-name/);
     expect(nome).toBeInTheDocument();
   });
-});*/
+
+  test('O peso médio do pokémon deve ser exibido', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+
+    const weight = getByTestId(/pokemon-weight/);
+    expect(weight).toBeInTheDocument();
+  });
+
+  test('A imagem deve conter um atributo src e alt', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+
+    const imagem = getAllByRole(/img/)[0];
+    expect(imagem).toBeInTheDocument();
+    expect(imagem).toHaveAttribute('src');
+    expect(imagem).toHaveAttribute('alt');
+  });
+});
