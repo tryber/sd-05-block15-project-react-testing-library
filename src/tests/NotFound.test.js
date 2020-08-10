ce9333dd47d62a62529cc1f4e53bc31a3b30247d
Elisa <elisatcamillo@gmail.com>
2020-08-10 02:28:20 -0300

    about, notfound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react'
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+test('page contains a h2 heading with the text "Page requested not found 😭"', () => {
+  const { container } = render(
+    <MemoryRouter initialEntries={['/blabla']}>
+      <App />
+    </MemoryRouter>
+  )
+
+  const h2 = container.querySelector('h2');
+  expect(h2).toBeInTheDocument();
+  expect(h2.textContent.trim()).toBe('Page requested not found 😭');
+});
+ 
+
+test('contains an specific image', () => {
+  const { getAllByRole } = render(
+    <MemoryRouter initialEntries={['/blabla']}>
+      <App />
+    </MemoryRouter>
+  )
+
+  const img = getAllByRole('img')
+  expect(img.length).toBe(2);
+
+  expect(img[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif')
+})
