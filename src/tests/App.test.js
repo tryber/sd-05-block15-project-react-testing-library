93bb50f803b093f4573235cadfa419c9b3658666
pauloricardoz <trybe.przt@gmail.com>
2020-08-10 00:56:22 -0300

    first test app.test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,11 +5,20 @@ import App from '../App';
 // start PR
 
 test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
+  const { getByText, getAllByRole } = render(
     <MemoryRouter>
       <App />
     </MemoryRouter>,
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+  const home = getByText(/home/i);
+  expect(home).toBeInTheDocument();
+  const about = getByText(/about/i);
+  expect(about).toBeInTheDocument();
+  const favorites = getByText(/Favorite/i);
+  expect(favorites).toBeInTheDocument();
+  
+
+
 });
