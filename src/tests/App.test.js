d39494da914b54c977664d4a20868c9966442b8d
pauloricardoz <trybe.przt@gmail.com>
2020-08-10 01:46:46 -0300

    re update test 1 with more seggregate tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,10 +12,31 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+test('renders a reading with the text `Home`', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
   const home = getByText(/home/i);
   expect(home).toBeInTheDocument();
+});
+test('renders a reading with the text `About`', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
   const about = getByText(/about/i);
   expect(about).toBeInTheDocument();
+});
+test('renders a reading with the text `Favorite`', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
   const favorites = getByText(/Favorite/i);
   expect(favorites).toBeInTheDocument();
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,7 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import { favoritePokemons } from '../components/FavoritePokemons';
+test('', () => {
+
+});
