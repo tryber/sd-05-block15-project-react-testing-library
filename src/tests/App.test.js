8b1c613f691f9bad3ca60055e96be1d083eab0b8
Andrevlc <andrevezi@gmail.com>
2020-08-10 01:37:18 -0300

    First attempt app.test.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,20 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 import App from '../App';
+
+
+function renderWithRouter(
+  ui,
+  { route = '/',
+  history = createMemoryHistory({ initialEntries: [route] }) } = {},
+  ) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -20,4 +33,30 @@ test('shows the Pokédex when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+describe('Testando navegação', () => {
+  afterEach(cleanup);
+
+  test('Navegando home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const goHome = getByText(/Encountered pokémons/i);
+    expect(goHome).toBeInTheDocument();
+    fireEvent.click(goHome);
+    expect(history.location.pathname).toBe('/');
+  });
+  test('Navegando about', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const goAbout = getByText(/This application simulates/i);
+    expect(goAbout).toBeInTheDocument();
+    fireEvent.click(goAbout);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  test('Navegando favoritos', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const goFav = getByText(/Favorite pokémons/i);
+    expect(goFav).toBeInTheDocument();
+    fireEvent.click(goFav);
+    expect(history.location.pathname).toBe('/favorites');
+  });
 });
