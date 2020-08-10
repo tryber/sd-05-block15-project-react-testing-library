819ab7f9c4b46e22dbf0536d7bb91db9528422a5
Thiago <asm.thiago@gmail.com>
2020-08-10 15:43:23 -0300

    Requisitos 3 e 4 OK

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../types/renderWithRouter';
+import pokemons from '../data';
+
+test('Caso a pessoa não tenha pokémons favoritos, a mensagem `No favorite pokemon found` deve aparecer na tela.', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons pokemons={[]} />);
+  const favoriteText = getByText(/No favorite pokemon found/i);
+  expect(favoriteText).toBeInTheDocument();
+});
+
+test('A página não deve exibir nenhum card de pokémon não favoritado.', () => {
+  const { getAllByText } = renderWithRouter(<FavoritePokemons pokemons={[pokemons[0]]} />);
+  expect(getAllByText('Pikachu').length).toEqual(1);
+  expect(getAllByText(/Average weight:/i).length).toEqual(1);
+});
