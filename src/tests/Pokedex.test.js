b766c1bc4e52017e3866f8b7e4c2c06959ac44fa
pauloricardoz <trybe.przt@gmail.com>
2020-08-10 04:13:47 -0300

    first Pokedex test

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,42 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import App from '../App';
+import pokemons from '../data';
+
+
+test('Testing Pokedex next buttom `Próximo pokémon`', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const nextButton = getByText('Próximo pokémon');
+  expect(nextButton).toBeInTheDocument();
+  const first = pokemons[0].name;
+  pokemons.forEach((pokemon) => {
+    const actualPoke = getByText(pokemon.name);
+    expect(actualPoke.innerHTML).not.toBe('');
+    fireEvent.click(nextButton);
+  });
+  const actualPoke = getByText(pokemons[0].name);
+  expect(actualPoke.innerHTML).toBe(first);
+});
+
+test('Checking type buttons', () => {
+  const { getAllByRole } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const allButtons = getAllByRole('button');
+  expect(allButtons.length).toBe(9);
+  expect(allButtons.some((btn) => btn.innerHTML === 'All')).toBe(true);
+  expect(allButtons.some((btn) => btn.innerHTML === 'Electric')).toBe(true);
+  expect(allButtons.some((btn) => btn.innerHTML === 'Fire')).toBe(true);
+  expect(allButtons.some((btn) => btn.innerHTML === 'Bug')).toBe(true);
+  expect(allButtons.some((btn) => btn.innerHTML === 'Poison')).toBe(true);
+  expect(allButtons.some((btn) => btn.innerHTML === 'Psychic')).toBe(true);
+  expect(allButtons.some((btn) => btn.innerHTML === 'Normal')).toBe(true);
+  expect(allButtons.some((btn) => btn.innerHTML === 'Dragon')).toBe(true);
+});
