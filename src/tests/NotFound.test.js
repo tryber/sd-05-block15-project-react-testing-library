a5ae56c0542bddb8eb6c67369f996c7d7046a32a
Andrevlc <andrevezi@gmail.com>
2020-08-11 15:41:54 -0300

    Requisite 4 - NotFound working but not checking for h2 tag, only text

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+// 4. Testes do arquivo NotFound.js
+// A página deve conter um heading h2 com o texto Page requested not found 😭;
+
+// A página deve exibir a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Teste NotFound page', () => {
+  test('Teste texto da página not found', () => {
+    const { getByText } = render(<NotFound />);
+    const notFoundText = getByText(/Page requested not found/i);
+    expect(notFoundText).toBeInTheDocument();
+  });
+  test('Testando se encontra o Gif correspondente da página', () => {
+    const { getByAltText } = render(<NotFound />);
+    const notFoundGif = getByAltText('Pikachu crying because the page requested was not found');
+    expect(notFoundGif).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
