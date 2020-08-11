c6ff7cc8921e1fdd4c73153b5760b019d29e7427
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-08-11 04:52:22 -0300

    Corrigindo um teste que não passava mais uma vez.

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -83,14 +83,16 @@ describe('Testes do arquivo Pokemon', () => {
   it('Ao clicar no link de navegação do pokémon, a aplicação deve ser redirecionada para a página de detalhes de pokémon. A URL exibida no navegador deve mudar para /pokemon/<id>, onde <id> é o id do pokémon cujos detalhes se deseja ver;', () => {
     const { id } = data.find(Boolean);
     const history = createMemoryHistory();
-    history.push(`/pokemons/${id}`);
-    const { getByRole } = render(
+    // history.push(`/pokemons/${id}`);
+    const { getAllByRole } = render(
       <Router history={history}>
-        <Pokemon pokemon={data.find(Boolean)} isFavorite={false} />
+        {/* <Pokemon pokemon={data.find(Boolean)} isFavorite={false} /> */}
+        <App />
       </Router>,
     );
-    const detailLink = getByRole('link');
+    const detailLink = getAllByRole('link')[3];
     fireEvent.click(detailLink);
+    console.log(history.location.pathname);
     expect(history.location.pathname).toBe(`pokemons/${id}`);
   });
 
