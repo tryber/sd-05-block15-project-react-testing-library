a66ff88c2be1d0f96946cb2df7c1e6b5abec7222
ambrosio <matmagamb1@gmail.com>
2020-08-11 15:13:23 -0300

    Favorite.test sem CC

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -3,7 +3,7 @@ import renderRoute from '../services/render';
 import { FavoritePokemons } from '../components';
 
 test('verify if has no pokemons', () => {
-    const { container } = renderRoute(<FavoritePokemons />);
-    const h2 = container.querySelectorAll('p');
-    expect(...h2).toHaveTextContent('No favorite pokemon found');
+  const { container } = renderRoute(<FavoritePokemons />);
+  const h2 = container.querySelectorAll('p');
+  expect(...h2).toHaveTextContent('No favorite pokemon found');
 });
