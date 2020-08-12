1321eeae5b7ee4bc818a920bf79d232a997b4f9b
Elisa <elisatcamillo@gmail.com>
2020-08-12 01:04:36 -0300

    codeclimate

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import { MemoryRouter } from 'react-router-dom'
-import { render, fireEvent, getAllByText } from '@testing-library/react'
-import App from '../App'
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import App from '../App';
 
 test('must render a button to load next pokemon', () => {
   const { getByText } = render(
@@ -10,7 +10,7 @@ test('must render a button to load next pokemon', () => {
     </MemoryRouter>,
   );
 
-  const prevUrl = getByText('More details').getAttribute('href') 
+  const prevUrl = getByText('More details').getAttribute('href');
   expect(getByText('Próximo pokémon'));
   fireEvent.click(getByText('Próximo pokémon'));
   expect(getByText('More details').getAttribute('href')).not.toEqual(prevUrl);
@@ -26,13 +26,17 @@ test('must show one pokemon at a time', () => {
   expect(getAllByText('More details').length).toBe(1);
   fireEvent.click(getByText('Próximo pokémon'));
   expect(getAllByText('More details').length).toBe(1);
-})
+});
 
 test('must show filter buttons', () => {
-  const { getAllByText, getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
+  const { getByText } = render(
+    <MemoryRouter>
       <App />
     </MemoryRouter>,
   );
-  
-})
+
+  const filters = ['All', 'Electric', 'gdfs', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+  for (let i = 0; i <= filters.length; i += 1) {
+    expect(getByText(filters[i])).toBeInTheDocument();
+  };
+});
