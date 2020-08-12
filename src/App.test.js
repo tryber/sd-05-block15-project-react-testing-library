fc448af783ec3ac52c278cbdd56af076ba4cce85
luialbeto <araujo.script@gmail.com>
2020-08-11 22:21:31 -0300

    testHomecc1

--- src/App.test.js
+++ src/App.test.js
@@ -3,14 +3,12 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
-{/* <Link className="link" to="/">{`Home`}</Link>
- */}
 test('testando HOME', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
-    </MemoryRouter>
-  )
+    </MemoryRouter>,
+  );
   const home = getByText(/Home/i);
   expect(home).toBeInTheDocument();
-})
+});
