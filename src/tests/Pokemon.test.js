7bf9bee6858a1b6b3720517b2b285683633a7281
Elisa <elisatcamillo@gmail.com>
2020-08-12 13:10:54 -0300

    pokemon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import App from '../App';
+import pokemons from '../data';
+
+test('returns a card with information about selected pokemon', () => {
+  const pid = 4;
+  const { getByTestId, getByText, getAllByRole } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  fireEvent.click(getByTestId('next-pokemon'));
+
+  const selectedPokemon = pokemons.find((pokemon) => pokemon.id === 4);
+  const { averageWeight, image, name } = selectedPokemon;
+
+  expect(getByTestId('pokemon-name')).toBeInTheDocument();
+  expect(getByTestId('pokemon-weight').innerHTML).toBe(`Average weight:${averageWeight.value}${averageWeight.measurementUnit}`);
+  expect(getAllByRole('img')[0]).toHaveProperty('src', image);
+  expect(getAllByRole('img')[0]).toHaveProperty('alt', `${name} sprite`);
+
+  expect(getByText('More details')).toHaveProperty('href', `http://localhost/pokemons/${pid}`);
+});
