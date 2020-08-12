ad45a6e08f529ff7150d20ee77b0ab2e9fabaa80
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-08-12 02:35:12 -0300

    Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, getByTestId } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
+import pokemons from '../data';
 
 test('Ao apertar o botão de próximo, a página deve exibir o próximo pokémon da lista', () => {
   // O botão deve conter o texto Próximo pokémon
@@ -44,7 +45,7 @@ test('Próximo Pokémon', () => {
   expect(proximoPokemon).toBeInTheDocument();
 });
 
-test('', () => {
+test('Pokemon Encontrado', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
@@ -52,4 +53,29 @@ test('', () => {
   );
   const pokemonEncontrado = getByText(/Encountered pokémon/i);
   expect(pokemonEncontrado).toBeInTheDocument();
+});
+
+test('Todos os tipos', () => {
+  const { getByText, getAllByTestId } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const typesButton = getAllByTestId('pokemon-type-button');
+  const nextButton = getByText(/Próximo pokémon/i);
+  typesButton.forEach((btn) => {
+    fireEvent.click(btn);
+    const selection = pokemons.filter((pokemon) => pokemon.type === btn.innerHTML);
+    const first = selection[0];
+    let count = 0;
+    selection.forEach((pokemon) => {
+      const pokemonAtual = getByText(pokemon.name);
+      expect(pokemonAtual.innerHTML).not.toBe('');
+      count += 1;
+      fireEvent.click(nextButton);
+    });
+    const pokemonAtual = getByText(selection[0].name);
+    expect(pokemonAtual.innerHTML).toBe(first.name);
+    expect(count).toBe(selection.length);
+  });
 });
