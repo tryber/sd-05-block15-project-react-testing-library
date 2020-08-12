3f00a7eb7b2ab69f0ab3d68b61b64bcc182e7f61
hleoc <informig@gmail.com>
2020-08-11 23:25:50 -0300

    Item 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import FavoritePokemons from '../components/FavoritePokemons';
 import renderWithRouter from '../components/renderWithRouter';
 
-
 test('A mensagem No favorite pokemon found deve aparecer na tela', () => {
   const { getByText } = renderWithRouter(<FavoritePokemons />);
   const favorit = getByText(/No favorite pokemon found/i);
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../components/renderWithRouter';
+
+test('A página deve conter um heading h2 e texto Page requested not found', () => {
+  const { getByRole, getByText } = renderWithRouter(<NotFound />);
+  const h2 = getByText(/Page requested not found/i);
+  expect(h2).toBeInTheDocument();
+  const elementoh2 = getByRole('heading', { tagName: 'h2' });
+  expect(elementoh2).toBeInTheDocument();
+});
+
+test('A página deve exibir a imagem x', () => {
+  const { getByAltText } = renderWithRouter(<NotFound />);
+  // const img = getByAltText('Pikachu crying because the page requested was not found');
+  // expect(img.src).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  expect(getByAltText('Pikachu crying because the page requested was not found')).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
