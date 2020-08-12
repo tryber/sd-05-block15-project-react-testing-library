da674038ed80f2632b4e069e5f9f7d8c7f17763a
luialbeto <araujo.script@gmail.com>
2020-08-12 01:39:48 -0300

    requisito pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,54 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import App from '../App';
+
+test('A página deve exibir o próximo pokémon da lista', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />;
+    </MemoryRouter>,
+  );
+  const button = getByText(/próximo/i);
+  fireEvent.click(button);
+  fireEvent.click(button);
+  fireEvent.click(button);
+  fireEvent.click(button);
+  fireEvent.click(button);
+  fireEvent.click(button);
+  fireEvent.click(button);
+  fireEvent.click(button);
+  fireEvent.click(button);
+  const Pikachu = getByText(/Pikachu/i);
+  expect(Pikachu).toBeInTheDocument();
+});
+
+test('Teste ID', () => {
+  const { getAllByTestId } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const testId = getAllByTestId('pokemon-type-button');
+  expect(testId.length).toBe(7);
+});
+
+test('Proximo Pokemon', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />;
+    </MemoryRouter>,
+  );
+  const proximoPokemon = getByText(/Próximo pokémon/i);
+  expect(proximoPokemon).toBeInTheDocument();
+});
+
+test('Encontrado Pokemon', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />;
+    </MemoryRouter>,
+  );
+  const encontradoPokemon = getByText(/Encountered pokémons/i);
+  expect(encontradoPokemon).toBeInTheDocument();
+});
