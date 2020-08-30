210f36df65e168345109a87485a9ccb547e36391
labelle <beelsdelonge@gmail.com>
2020-08-30 19:45:55 -0300

    CC atrapalhando de novo

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+//import React from 'react';
+//import { MemoryRouter } from 'react-router-dom';
+//import { render } from '@testing-library/react';
 
 test('', () => {});
