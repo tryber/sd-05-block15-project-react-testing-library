a56ceedc58f287467c74de8c57e0963b59dc05c9
labelle <beelsdelonge@gmail.com>
2020-08-30 19:17:41 -0300

    NotFound com ajuda do Kyle

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from "react";
+import { render } from "@testing-library/react";
+import NotFound from '../components/NotFound';
+import { MemoryRouter } from "react-router-dom";
+
+test('Heading e Imagem', () => {
+  const { getByText, container } = render(
+  <MemoryRouter>  
+    <NotFound />
+  </MemoryRouter>,
+  );
+  const heading = getByText('Page requested not found');
+  expect(heading).toBeInTheDocument();
+  expect(heading.tagName).toBe('H2');
+  const imagem = container.querySelector('img');
+  expect(imagem.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
