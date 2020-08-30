e36e978d78bd67630bd4d2ddb08538ab2abffbdc
labelle <beelsdelonge@gmail.com>
2020-08-30 19:19:24 -0300

    Corrigindo CC

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
-import About from '../components/About';
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
+import { render } from "@testing-library/react";
+import About from "../components/About";
 
-test('exibir informações sobre a Pokedéx', () => {
-    const { getByText, container, getByRole } = render(
+test("exibir informações sobre a Pokedéx", () => {
+  const { getByText, container, getByRole } = render(
     <MemoryRouter>
       <About />
     </MemoryRouter>,
-    );
-    const aboutPokedex = getByText(/About/);
-    expect(aboutPokedex).toBeInTheDocument();
-    expect(aboutPokedex.tagName).toBe('H2');
-    const paragraphs = container.querySelectorAll('p');
-    expect(paragraphs.length).toBe(2);
-    const imagem = getByRole('img');
-    expect(imagem.src).toBe(`https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png`);
+  );
+  const aboutPokedex = getByText(/About/);
+  expect(aboutPokedex).toBeInTheDocument();
+  expect(aboutPokedex.tagName).toBe("H2");
+  const paragraphs = container.querySelectorAll("p");
+  expect(paragraphs.length).toBe(2);
+  const imagem = getByRole("img");
+  expect(imagem.src).toBe(
+    `https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png`,
+  );
 });
