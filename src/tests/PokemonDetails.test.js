870e7a40c26ff6db1c88277502fbb8b68b3c4891
labelle <beelsdelonge@gmail.com>
2020-09-02 18:53:19 -0300

    tentando corrigir a mutaçao parte 2

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -11,7 +11,7 @@ test('PokemonDetails page test', () => {
   const favoritoPokemon = Data.find(Boolean);
   const history = createMemoryHistory();
   history.push(`/pokemons/${favoritoPokemon.id}`);
-  const { queryByText, getByText, queryByLabelText, queryAllByRole } = render(
+  const { queryByText, getByText, queryByLabelText, queryAllByRole, findByText } = render(
     <Router history={history}>
       <App />
     </Router>,
@@ -20,7 +20,7 @@ test('PokemonDetails page test', () => {
   const pokemonName = getByText(`${favoritoPokemon.name} Details`);
   expect(pokemonName).toBeInTheDocument();
 
-  const heading = getByText(/Summary/);
+  const heading = findByText(/Summary/);
   expect(heading).toBeInTheDocument();
   expect(heading.tagName).toBe('H2');
 
