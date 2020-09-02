a2b4e337a52ee5e2e64d92e4b0a36bcb21a07f65
labelle <beelsdelonge@gmail.com>
2020-09-02 19:25:58 -0300

    ajustando codeclimates

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { MemoryRouter, Router } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
-
-import { createMemoryHistory } from 'history';
-
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -22,35 +19,22 @@ test('shows the Pokédex when the route is `/`', () => {
       <App />
     </MemoryRouter>,
   );
-  
+
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
 
 test('links on the page', () => {
-  const { getByText, queryAllByRole } = render(
+  const { queryAllByRole } = render(
     <MemoryRouter>
       <App />
     </MemoryRouter>,
   );
-  
+
   const links = queryAllByRole('link');
   expect(links[0].text).toBe('Home');
   expect(links[1].text).toBe('About');
   expect(links[2].text).toBe('Favorite Pokémons');
-  expect(links[0].pathname).toBe(`/`);
+  expect(links[0].pathname).toBe('/');
   expect(links[1].pathname).toBe('/about');
   expect(links[2].pathname).toBe('/favorites');
-  
-});
-
-test('page Not Found', () => {
-  const history = createMemoryHistory();
-  history.push('/batatinha');
-  const { getByText, container } = render(
-    <Router history={history}>
-      <App />
-    </Router>,
-  );
-  const heading = getByText('Page requested not found');
-  expect(heading).toBeInTheDocument();
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { MemoryRouter, Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 
-import { createMemoryHistory} from 'history';
+import { createMemoryHistory } from 'history';
 
 import App from '../App';
 
