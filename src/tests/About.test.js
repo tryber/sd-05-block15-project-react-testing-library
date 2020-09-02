2b5ca340eeb3dae6a9b64bd90adced5c746eddcf
luca <luucadecastro@gmail.com>
2020-09-01 23:10:22 -0300

    cc 1

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import About from '../components/About';
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import About from '../components/About';
 
-test('renders a text with information about Pokedéx', () => {
-  const { getByText } = render(
-    <About />
-  )
-});
+// test('renders a text with information about Pokedéx', () => {
+//   const { getByText } = render(
+//     <About />
+//   )
+// });
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,57 +1,57 @@
-import React from "react";
-import { MemoryRouter } from "react-router-dom";
-import { render, fireEvent } from "@testing-library/react";
-import App from "../App";
-import { createMemoryHistory } from "history";
-import NotFound from "../components/NotFound";
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render, fireEvent } from '@testing-library/react';
+import App from '../App';
+import NotFound from '../components/NotFound';
 
-test("renders a reading with the text `Pokédex`", () => {
+test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
-    </MemoryRouter>
+    </MemoryRouter>,
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
-test("Ao carregar a aplicação no caminho de URL “/”, a página principal da Pokédex deve ser mostrada.", () => {
+test('Ao carregar a aplicação no caminho de URL “/”, a página principal da Pokédex deve ser mostrada.', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
-    </MemoryRouter>
+    </MemoryRouter>,
   );
-  createMemoryHistory("/");
+  createMemoryHistory('/');
   const page = getByText(/Encountered pokémons/i);
   expect(page).toBeInTheDocument();
 });
-describe("No topo da aplicação, deve haver um conjunto fixo de links de navegação", () => {
-  test("O primeiro link deve possuir o texto Home com a URL /", () => {
+describe('No topo da aplicação, deve haver um conjunto fixo de links de navegação', () => {
+  test('O primeiro link deve possuir o texto Home com a URL /', () => {
     const { getByText } = render(
       <MemoryRouter>
         <App />
-      </MemoryRouter>
+      </MemoryRouter>,
     );
-    createMemoryHistory("/");
+    createMemoryHistory('/');
     const Home = getByText(/Home/i);
     expect(Home).toBeInTheDocument();
   });
-  test("O segundo link deve possuir o texto About com a URL /about", () => {
+  test('O segundo link deve possuir o texto About com a URL /about', () => {
     const { getByText } = render(
       <MemoryRouter>
         <App />
-      </MemoryRouter>
+      </MemoryRouter>,
     );
-    createMemoryHistory("/about");
+    createMemoryHistory('/about');
     const About = getByText(/About/i);
     expect(About).toBeInTheDocument();
   });
-  test("O terceiro link deve possuir o texto Favorite Pokémons com a URL /favorites", () => {
+  test('O terceiro link deve possuir o texto Favorite Pokémons com a URL /favorites', () => {
     const { getByText } = render(
       <MemoryRouter>
         <App />
-      </MemoryRouter>
+      </MemoryRouter>,
     );
-    createMemoryHistory("/favorites");
+    createMemoryHistory('/favorites');
     const Favorites = getByText(/Favorites/i);
     expect(Favorites).toBeInTheDocument();
   });
@@ -60,40 +60,40 @@ test('Ao clicar no link "Home" na barra de navegação, a aplicação deve ser r
   const { getByText } = render(
     <MemoryRouter>
       <App />
-    </MemoryRouter>
+    </MemoryRouter>,
   );
-  const history = createMemoryHistory("/");
+  const history = createMemoryHistory('/');
   const Home = getByText(/Home/i);
   fireEvent.click(Home);
-  expect(history.location.pathname).toBe("/");
+  expect(history.location.pathname).toBe('/');
 });
 test('Ao clicar no link "About" na barra de navegação, a aplicação deve ser redirecionada para a página de About, na URL "/about"', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
-    </MemoryRouter>
+    </MemoryRouter>,
   );
-  const history = createMemoryHistory("/about");
+  const history = createMemoryHistory('/about');
   const About = getByText(/About/i);
   fireEvent.click(About);
-  expect(history.location.pathname).toBe("/about");
+  expect(history.location.pathname).toBe('/about');
 });
 test('Ao clicar no link "Favorite Pokémons" na barra de navegação, a aplicação deve ser redirecionada para a página de pokémons favoritados, na URL "/favorites"', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
-    </MemoryRouter>
+    </MemoryRouter>,
   );
-  const history = createMemoryHistory("/favorites");
+  const history = createMemoryHistory('/favorites');
   const favoritePokémons = getByText(/Favorite Pokémons/i);
   fireEvent.click(favoritePokémons);
-  expect(history.location.pathname).toBe("/favorites");
+  expect(history.location.pathname).toBe('/favorites');
 });
-test("Entrar em uma URL desconhecida exibe a página Not Found", () => {
+test('Entrar em uma URL desconhecida exibe a página Not Found', () => {
   const { getByText } = render(
     <MemoryRouter>
       <NotFound />
-    </MemoryRouter>
+    </MemoryRouter>,
   );
   expect(getByText(/page request not found/i)).toBeInTheDocument();
 });
