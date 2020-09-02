d1208beb5e7643c2518b7d73a235c3fa3822cb71
luca <luucadecastro@gmail.com>
2020-09-02 00:42:00 -0300

    cc

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,31 +1,31 @@
-import React from "react";
-import { createMemoryHistory } from "history";
-import About from "../components/About";
-import renderWithRouter from "./App.test";
+import React from 'react';
+import { createMemoryHistory } from 'history';
+import About from '../components/About';
+import renderWithRouter from './App.test';
 
 test('A página "About" deve exibir informações sobre a Pokédex', () => {
-  const { getByText, container } = renderWithRouter(<About />);
+  const { getByText } = renderWithRouter(<About />);
   const history = createMemoryHistory();
-  history.push("/about");
+  history.push('/about');
   expect(getByText(/about pokédex/i)).toBeInTheDocument();
 });
 
-test("A página deve conter um heading h2 com o texto About Pokédex", () => {
-  const { getByText, container } = renderWithRouter(<About />);
+test('A página deve conter um heading h2 com o texto About Pokédex', () => {
+  const { container } = renderWithRouter(<About />);
   const history = createMemoryHistory();
-  history.push("/about");
+  history.push('/about');
   expect(container.innerHTML.includes("h2"));
 });
 
-test("A página deve conter dois parágrafos com texto sobre a Pokédex", () => {
+test('A página deve conter dois parágrafos com texto sobre a Pokédex', () => {
   const { container } = renderWithRouter(<About />);
-  expect(container.querySelectorAll("p").length);
+  expect(container.querySelectorAll('p').length);
 });
 
-test("A página deve conter a imagem de uma Pokédex:", () => {
+test('A página deve conter a imagem de uma Pokédex:', () => {
   const { getByRole } = renderWithRouter(<About />);
   expect(getByRole(/img/i)).toHaveAttribute(
-    "src",
-    "https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png"
+    'src',
+    'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
   );
 });
