95b236ce2149f616d40cf0036aa6f54042a6427e
luca <luucadecastro@gmail.com>
2020-09-02 15:48:25 -0300

    cc

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -28,8 +28,8 @@ describe('Deve conter mais informações sobre apenas o pokémon selecionado;', 
   });
 
   test('A seção de detalhes deve conter um heading h2 com o texto Summary;', () => {
-    const history = createMemoryHistory(); 
-    history.push('pokemon/25');
+    const history = createMemoryHistory();
+    history.push('pokemons/25');
     const { getAllByRole } =
     render(
       <MemoryRouter>
@@ -38,16 +38,16 @@ describe('Deve conter mais informações sobre apenas o pokémon selecionado;', 
     );
     const h2 = getAllByRole('heading')[2];
     expect(h2.tagName).toBe('h2');
-    expect(h2.innerHTML).toBe('Summary');      
+    expect(h2.innerHTML).toBe('Summary');
   });
 
   test('A seção de detalhes deve conter um parágrafo com o resumo do pokémon específico sendo visualizado', () => {
     const history = createMemoryHistory();
     history.push('/pokemons/25');
     const { container } = render(
-      <Router history={history}>
+      <MemoryRouter history={history}>
         <App />
-      </Router>,
+      </MemoryRouter>,
     );
     const resumo = container.querySelectorAll('p')[3];
     expect(resumo.innerHTML).toBe(
@@ -55,57 +55,57 @@ describe('Deve conter mais informações sobre apenas o pokémon selecionado;', 
     );
   });
 });
-  describe('A página de detalhes deve exibir uma seção com os mapas com as localizações do pokémon', () => {
-    test('A seção de detalhes deve conter um heading h2 com o texto Game Locations of <name>, , onde <name> é o nome do pokémon exibido', () => {
-      const history = createMemoryHistory();
-      history.push('/pokemons/25');
-      const { queryByText } = render(
-        <Router history={history}>
-          <App />
-        </Router>,
-      );
-      const gameLocation = queryByText(/Game Locations of/);
-      expect(gameLocation.innerHTML).toBe('Game Locations of Pikachu');
-    });
-    test('A seção de detalhes deve exibir todas as localizações do pokémon', () => {
-      const history = createMemoryHistory();
-      history.push('/pokemons/25');
-      const { queryByText } = render(
-        <Router history={history}>
-          <App />
-        </Router>,
-      );
-      const pikachu = pokemons[0];
-      pikachu.foundAt.forEach((local) => {
-        const location = queryByText(local.location);
-        expect(location.innerHTML).toBe(local.location);
-      });
-    });
-    test('A imagem da localização deve ter um atributo src com a URL da localização', () => {
-      const history = createMemoryHistory();
-      history.push('/pokemons/25');
-      const { getAllByAltText } = render(
-        <Router history={history}>
-          <App />
-        </Router>,
-      );
-      const locationsMap = getAllByAltText(/Pikachu location/);
-      expect(locationsMap[0].src).toBe(
-        'https://cdn.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
-      );
-      expect(locationsMap[1].src).toBe(
-        'https://cdn.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
-      );
-    });
-  });
-    test('O label do checkbox deve ser Pokémon favoritado?', () => {
-      const history = createMemoryHistory();
-      history.push('/pokemons/25');
-      const { getByLabelText } = render(
-        <Router history={history}>
-          <App />
-        </Router>,
+describe('A página de detalhes deve exibir uma seção com os mapas com as localizações do pokémon', () => {
+  test('A seção de detalhes deve conter um heading h2 com o texto Game Locations of <name>, , onde <name> é o nome do pokémon exibido', () => {
+    const history = createMemoryHistory();
+    history.push('/pokemons/25');
+    const { queryByText } = render(
+      <MemoryRouter history={history}>
+        <App />
+      </MemoryRouter>,
     );
-    const checkbox = getByLabelText('Pokémon favoritado?');
-    expect(checkbox).toBeInTheDocument();
+    const gameLocation = queryByText(/Game Locations of/);
+    expect(gameLocation.innerHTML).toBe('Game Locations of Pikachu');
   });
+  test('A seção de detalhes deve exibir todas as localizações do pokémon', () => {
+    const history = createMemoryHistory();
+    history.push('/pokemons/25');
+    const { queryByText } = render(
+      <MemoryRouter history={history}>
+        <App />
+      </MemoryRouter>,
+    );
+    const pikachu = pokemons[0];
+    pikachu.foundAt.forEach((local) => {
+      const location = queryByText(local.location);
+      expect(location.innerHTML).toBe(local.location);
+    });
+  });
+  test('A imagem da localização deve ter um atributo src com a URL da localização', () => {
+    const history = createMemoryHistory();
+    history.push('/pokemons/25');
+    const { getAllByAltText } = render(
+      <MemoryRouter history={history}>
+        <App />
+      </MemoryRouter>,
+    );
+    const locationsMap = getAllByAltText(/Pikachu location/);
+    expect(locationsMap[0].src).toBe(
+      'https://cdn.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+    );
+    expect(locationsMap[1].src).toBe(
+      'https://cdn.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+    );
+  });
+});
+test('O label do checkbox deve ser Pokémon favoritado?', () => {
+  const history = createMemoryHistory();
+  history.push('/pokemons/25');
+  const { getByLabelText } = render(
+    <MemoryRouter history={history}>
+      <App />
+    </MemoryRouter>,
+  );
+      const checkbox = getByLabelText('Pokémon favoritado?');
+      expect(checkbox).toBeInTheDocument();
+});
