bd83279bdfaf892804995260420a8d6aabdd8354
Larissa Palombo <larissampalombo@gmail.com>
2020-09-11 14:23:01 -0300

    not found

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+test('Mensagem Not found', () => {
+  const { getByText } = render(
+    <MemoryRouter}>
+      <NotFound />
+    </MemoryRouter>,
+  );
+  const notFound = getByText(/Page requested not found 😭/i);
+  expect(notFound).toBeInTheDocument('');
+});
+
+test('Testar imagem NotFound', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const img = getByRole('img');
+  
+    expect(img).toBeInTheDocument();
+  
+    expect(img.src).not.toBe('');
+  });
+  
