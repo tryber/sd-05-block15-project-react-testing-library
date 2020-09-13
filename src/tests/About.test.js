0a4ee349d7399826ff2b24976e0983ccc0b4549a
Sidnei Ramos <sidneiramos02@gmail.com>
2020-09-13 12:59:38 -0300

    Testando o About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,26 +1,17 @@
-// import React from 'react';
-// import { MemoryRouter } from 'react-router-dom';
-// import { render } from '@testing-library/react';
-// import App from '../App';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
 
-// test('Verificar se existe "About"', () => {
-//   const { getByText } = render(
-//     <MemoryRouter initialEntries={['/about']}>
-//       <App />
-//     </MemoryRouter>,
-//   );
-
-//   const about = getByText(/About Pokedéx/i);
-//   expect(getByText(about)).toBeInTheDocument();
-// });
-
-// test('Verificar se existe "About"', () => {
-//     const { getBy } = render(
-//       <MemoryRouter initialEntries={['/']}>
-//         <App />
-//       </MemoryRouter>,
-//     );
-
-//     const about = getByText(/About Pokedéx/i);
-//     expect(getByText(about)).toBeInTheDocument();
-//   });
+test('Testando tudo no About', () => {
+  const { getByText, getByRole } = render(
+    <MemoryRouter initialEntries={['/about']}>
+      <App />
+    </MemoryRouter>,
+  );
+  const about = getByText(/About Pokédex/i);
+  const imagem = getByRole('img');
+  expect(about).toBeInTheDocument();
+  expect(imagem).toBeInTheDocument();
+  expect(imagem.src).not.toBe('');
+});
