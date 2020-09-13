77ba88663430417a5e9cee4e81c2d94a683cccae
Sidnei Ramos <sidneiramos02@gmail.com>
2020-09-13 12:27:03 -0300

    CC 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
-import App from '../App';
+// import React from 'react';
+// import { MemoryRouter } from 'react-router-dom';
+// import { render } from '@testing-library/react';
+// import App from '../App';
 
-test('Verificar se existe "About"', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/about']}>
-      <App />
-    </MemoryRouter>,
-  );
+// test('Verificar se existe "About"', () => {
+//   const { getByText } = render(
+//     <MemoryRouter initialEntries={['/about']}>
+//       <App />
+//     </MemoryRouter>,
+//   );
 
-  const about = getByText(/About Pokedéx/i);
-  expect(getByText(about)).toBeInTheDocument();
-});
+//   const about = getByText(/About Pokedéx/i);
+//   expect(getByText(about)).toBeInTheDocument();
+// });
 
 test('Verificar se existe "About"', () => {
     const { getBy } = render(
