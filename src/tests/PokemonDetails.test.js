b410cb560de190f149d0a81280fd9f101244e1ed
Sidnei Ramos <sidneiramos02@gmail.com>
2020-09-13 23:57:47 -0300

    Finalmente. Sera?

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from '../App';
 
-test('Tentando conjunto de links de verificação', () => {
-  const { getByText } = render(
+test('Testando o More Details', () => {
+  const { getByText, getAllByAltText, getByLabelText } = render(
     <MemoryRouter>
       <App />
     </MemoryRouter>,
   );
-  const heading = getByText(/Pokédex/i);
-  const favorito = getByText(/Favorite Pokémons/i);
-  const home = getByText(/Home/i);
-  const about = getByText(/About/i);
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
-  expect(heading).toBeInTheDocument();
-  expect(favorito).toBeInTheDocument();
-  expect(home).toBeInTheDocument();
-  expect(about).toBeInTheDocument();
+
+  fireEvent.click(getByText('More details'));
+  expect(getByText('Pikachu Details')).toBeInTheDocument();
+  expect(getByText(/summary/i)).toBeInTheDocument();
+  expect(getByText(/summary/i).innerHTML).toBe(' Summary ');
+  expect(getByText(/summary/i).tagName).toBe('H2');
+  expect(getByText(/game locations of pikachu/i)).toBeInTheDocument();
+  expect(getByText(/game locations of pikachu/i).innerHTML).toBe(
+    'Game Locations of Pikachu',
+  );
+  expect(getByText(/game locations of pikachu/i).tagName).toBe('H2');
+  expect(
+    getByText(
+      /This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat./i,
+    ),
+  ).toBeInTheDocument();
+  expect(getAllByAltText(/Pikachu location/i)[0].src).toBe(
+    'https://cdn.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+  );
+  expect(getAllByAltText(/Pikachu location/i)[1].src).toBe(
+    'https://cdn.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+  );
+  expect(getByText(/kanto viridian forest/i)).toBeInTheDocument();
+  expect(getByText(/kanto power plant/i)).toBeInTheDocument();
+  expect(getByLabelText('Pokémon favoritado?')).toBeInTheDocument;
 });
