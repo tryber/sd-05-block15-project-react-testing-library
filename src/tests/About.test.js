b17512be78acd844ccbf2475b45bebdb82cdfd61
SamuelPPlab <samuelpp.s@hotmail.com>
2020-10-06 21:00:53 -0300

    primeiro requisito pronto eu imagino

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+
+describe('teste das rotas', () => {
+  test('home', () => {
+    const { getByText } = render(<App />);
+    expect(getByText(/home/i)).toBeInTheDocument();
+  })
+  test('about', () => {
+
+  })
+  test('favorite pokemons', () => {
+
+  })
+})
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history'
+import { MemoryRouter, Router } from 'react-router-dom';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -12,3 +13,35 @@ test('renders a reading with the text `Pokédex`', () => {
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
+
+describe('teste das rotas', () => {
+  afterEach(cleanup);
+  test('home', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    expect(getByText(/home/i)).toBeInTheDocument();
+  })
+  test('about', () => {
+    const history = createMemoryHistory();
+    const { getByText } = render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+    fireEvent.click(getByText(/about/i));
+    expect(history.location.pathname).toBe('/about');
+  })
+  test('favorite pokemons', () => {
+    const history = createMemoryHistory();
+    const { getByText } = render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+    fireEvent.click(getByText(/favorite pokémons/i));
+    expect(history.location.pathname).toBe('/favorites')
+  })
+})
